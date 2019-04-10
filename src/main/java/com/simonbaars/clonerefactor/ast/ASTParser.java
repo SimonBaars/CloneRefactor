1dba2be7bb78cc06fc0a341e974e7e6be5cf7937
Simon Baars <simon.mailadres@gmail.com>
2019-04-10 14:38:05 +0200

    Fixed memory overhead problems
    Watch out with static stuff boys

--- src/main/java/com/simonbaars/clonerefactor/ast/ASTParser.java
+++ src/main/java/com/simonbaars/clonerefactor/ast/ASTParser.java
@@ -22,24 +22,24 @@ import com.simonbaars.clonerefactor.model.Location;
 import com.simonbaars.clonerefactor.model.Sequence;
 
 public class ASTParser {
-	private static final ListMap<Integer, Location> cloneReg = new ListMap<>();
 
 	public static List<Sequence> parse(List<File> javaFiles) {
-		Location lastLoc = calculateLineReg(javaFiles);
+		final ListMap<Integer, Location> cloneReg = new ListMap<>();
+		Location lastLoc = calculateLineReg(javaFiles, cloneReg);
 		if(lastLoc!=null)
 			return new CloneDetection().findChains(lastLoc, cloneReg);
 		return new ArrayList<>();
 	}
 
-	private static final Location calculateLineReg(List<File> javaFiles) {
+	private static final Location calculateLineReg(List<File> javaFiles, ListMap<Integer, Location> cloneReg) {
 		final Map<LineTokens, Location> lineReg = new HashMap<>();
 		Location l = null;
 		final CompilationUnitReg r = new CompilationUnitReg();
 		for(File file : javaFiles) {
 			try {
-				l = setIfNotNull(l, parseClassFile(lineReg, file, r));
+				l = setIfNotNull(l, parseClassFile(lineReg, file, r, cloneReg));
 			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+				return null;
 			}
 			r.reset();
 		}
@@ -50,14 +50,13 @@ public class ASTParser {
 		return parseClassFile == null ? l : parseClassFile;
 	}
 
-	private static Location parseClassFile(final Map<LineTokens, Location> lineReg, File file, CompilationUnitReg r)
-			throws FileNotFoundException {
+	private static Location parseClassFile(final Map<LineTokens, Location> lineReg, File file, CompilationUnitReg r, ListMap<Integer, Location> cloneReg) throws FileNotFoundException {
 		final ParseResult<CompilationUnit> pr = new JavaParser().parse(file);
 		if(pr.isSuccessful() && pr.getResult().isPresent()) {
 			CompilationUnit cu = pr.getResult().get();
 			Location l = null;
 			for (Iterator<Node> it = cu.stream().iterator(); it.hasNext();) {
-				l = parseToken(lineReg, file, r, it);
+				l = parseToken(lineReg, file, r, it, cloneReg);
 			}
 			return l;
 		}
@@ -65,29 +64,26 @@ public class ASTParser {
 	}
 
 	private static Location parseToken(final Map<LineTokens, Location> lineReg, File file, final CompilationUnitReg r,
-			Iterator<Node> it) {
+			Iterator<Node> it, ListMap<Integer, Location> cloneReg) {
 		Node t = it.next();
 		Optional<Range> range = t.getRange();
 		Location l = null;
 		if(range.isPresent()) {
 			int line = range.get().begin.line;
 			if(r.lastLineNumberExists() && r.getLastLineNumber()!=line)
-				l = addLineTokensToReg(lineReg, file, r, it, line);
+				l = addLineTokensToReg(lineReg, file, r, r.getLastLineNumber(), cloneReg);
 			r.visitLine(line);
 			r.getThisLine().add(t);
 			if(!it.hasNext())
-				l = addLineTokensToReg(lineReg, file, r, it, line);
+				l = addLineTokensToReg(lineReg, file, r, line, cloneReg);
 		}
 		return l;
 	}
 
 	private static Location addLineTokensToReg(final Map<LineTokens, Location> lineReg, File file,
-			final CompilationUnitReg r, Iterator<Node> it, int line) {
-		int finishedLine = line; // Line to be scanned for clones
-		if(it.hasNext())
-			finishedLine = r.getLastLineNumber();
+			final CompilationUnitReg r, int line, ListMap<Integer, Location> cloneReg) {
 		LineTokens l = new LineTokens(r.getThisLine());
-		Location location = new Location(file, finishedLine, l.size(), l.hashCode());
+		Location location = new Location(file, line, l.size(), l.hashCode());
 		cloneReg.addTo(location.getTokenHash(), location);
 		if(lineReg.containsKey(l)) {
 			location.setClone(lineReg.get(l));
