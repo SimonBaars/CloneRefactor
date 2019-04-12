1fc305493a58ba663b0b7669bd9832ed630a5c73
Simon Baars <simon.mailadres@gmail.com>
2019-04-12 11:27:07 +0200

    Changing things

--- src/main/java/com/simonbaars/clonerefactor/ast/ASTParser.java
+++ src/main/java/com/simonbaars/clonerefactor/ast/ASTParser.java
@@ -1,18 +1,18 @@
 package com.simonbaars.clonerefactor.ast;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.Node;
 import com.simonbaars.clonerefactor.datatype.ListMap;
-import com.simonbaars.clonerefactor.model.CompilationUnitReg;
 import com.simonbaars.clonerefactor.model.LineTokens;
 import com.simonbaars.clonerefactor.model.Location;
 
 public class ASTParser implements Parser {
-	final 
+	final Map<LineTokens, Location> lineReg = new HashMap<>();
 	
 	public Location parseToken(Location prevLocation, File file, Node t, ListMap<Integer, Location> cloneReg) {
 		Optional<Range> range = t.getRange();
@@ -21,32 +21,29 @@ public class ASTParser implements Parser {
 			int line = range.get().begin.line;
 			if(prevLocation.getLine() != line) {
 				thisLocation = new Location(file, line, prevLocation);
+				prevLocation.setNextLine(thisLocation);
 			} else thisLocation = prevLocation;
+			if()
 			addLineTokensToReg(thisLocation, cloneReg);
-			r.visitLine(line);
-			r.getThisLine().add(t);
-			//if(!it.hasNext())
-			//	l = addLineTokensToReg(lineReg, file, r, line, cloneReg);
 		}
 		return thisLocation;
 	}
 
 	private Location addLineTokensToReg(Location location, ListMap<Integer, Location> cloneReg) {
 		cloneReg.addTo(location.getTokenHash(), location);
-		if(lineReg.containsKey(l)) {
-			location.setClone(lineReg.get(l));
-			lineReg.put(l, location);
+		if(lineReg.containsKey(location.getTokens())) {
+			location.setClone(lineReg.get(location.getTokens()));
+			lineReg.put(location.getTokens(), location);
 		} else {
-			lineReg.put(l, location);
+			lineReg.put(location.getTokens(), location);
 		}
-		r.nextLine(location);
 		return location;
 	}
 	
-	public Location extractLinesFromAST(Location prevLocation, File file, Node n, ListMap<Integer, Location> cloneReg) {
-		prevLocation = parseToken(prevLocation, file, r,  n, cloneReg);
+	public Location extractLinesFromAST(Location prevLocation, File file, Node n, ListMap<Integer, Location> cloneReg, boolean isLast) {
+		prevLocation = parseToken(prevLocation, file,  n, cloneReg);
 		for (Node child : n.getChildNodes()) {
-			prevLocation = setIfNotNull(prevLocation, extractLinesFromAST(prevLocation, file, r, child, cloneReg));
+			prevLocation = setIfNotNull(prevLocation, extractLinesFromAST(prevLocation, file, child, cloneReg));
 		}
 		return prevLocation;
 	}
