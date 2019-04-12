449b2360a6a93d14f16b4b43788595d1e54d159a
Simon Baars <simon.mailadres@gmail.com>
2019-04-12 19:23:22 +0200

    Just doin' some experiments in master branch :speak_no_evil:

--- src/main/java/com/simonbaars/clonerefactor/ast/NodeParser.java
+++ src/main/java/com/simonbaars/clonerefactor/ast/NodeParser.java
@@ -10,10 +10,12 @@ import java.util.Optional;
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.nodeTypes.NodeWithBody;
+import com.github.javaparser.ast.nodeTypes.NodeWithIdentifier;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.simonbaars.clonerefactor.model.LineTokens;
 import com.simonbaars.clonerefactor.model.Location;
@@ -22,7 +24,7 @@ public class NodeParser implements Parser {
 	final Map<LineTokens, Location> lineReg = new HashMap<>();
 	
 	public Location extractLinesFromAST(Location prevLocation, File file, Node n) {
-		if(n instanceof ImportDeclaration || n instanceof Expression)
+		if(n instanceof ImportDeclaration || n instanceof Expression || n instanceof Modifier || n instanceof NodeWithIdentifier)
 			return prevLocation;
 		if(!(n instanceof CompilationUnit))
 			prevLocation = parseToken(prevLocation, file,  n);
@@ -45,7 +47,7 @@ public class NodeParser implements Parser {
 	
 	
 	public Location parseToken(Location prevLocation, File file, Node n) {
-		System.out.println("Parsing "+n.getClass()+" as "+n);
+		//System.out.println("Parsing "+n.getClass()+" as "+n);
 		Optional<Range> range = n.getRange();
 		Location thisLocation = prevLocation;
 		if(range.isPresent()) {
