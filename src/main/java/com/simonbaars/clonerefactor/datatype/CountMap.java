e2b6bcfed70d697d485f034402bff664b9686783
Simon Baars <simon.mailadres@gmail.com>
2019-04-17 07:34:01 +0200

    This fancy countmap implementation

---
+++ src/main/java/com/simonbaars/clonerefactor/datatype/CountMap.java
@@ -0,0 +1,38 @@
+package com.simonbaars.clonerefactor.datatype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountMap<K> extends HashMap<K, Integer> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CountMap() {
+	}
+
+	public CountMap(int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+	}
+
+	public CountMap(int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	public CountMap(Map<? extends K, Integer> m) {
+		super(m);
+	}
+	
+	public Integer increment(K key) {
+        return super.put(key, super.containsKey(key) ? super.get(key) + 1 : 1);
+    }
+	
+	@SuppressWarnings("unchecked")
+	@Override 
+	public Integer get(Object key){
+		if(!super.containsKey(key))
+			super.put((K) key, 0);
+		return super.get(key);
+	}
+}
