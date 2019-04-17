d8defab8fa42e946cb0f2a2c84d22f5e0423a5da
Simon Baars <simon.mailadres@gmail.com>
2019-04-17 07:35:05 +0200

    Wrote the found node part of the metric collector

--- src/main/java/com/simonbaars/clonerefactor/metrics/MetricCollector.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/MetricCollector.java
@@ -1,5 +1,44 @@
 package com.simonbaars.clonerefactor.metrics;
 
-public class MetricCollector {
+import java.io.File;
+import java.util.List;
 
+import com.simonbaars.clonerefactor.datatype.ListMap;
+import com.simonbaars.clonerefactor.model.Location;
+import com.simonbaars.clonerefactor.model.Sequence;
+
+public class MetricCollector {
+	ListMap<File, Integer> parsedLines = new ListMap<>();
+	Metrics metrics = new Metrics();
+	
+	public MetricCollector() {}
+	
+	public void reportFoundNode(Location l) {
+		metrics.totalAmountOfLines+=getUnparsedLines(l);
+		metrics.totalAmountOfNodes+=l.getAmountOfNodes();
+		metrics.totalAmountOfTokens+=l.getAmountOfTokens();
+	}
+	
+	private int getUnparsedLines(Location l) {
+		int amountOfLines = 0;
+		for(int i = l.getRange().begin.line; i<=l.getRange().end.line; i++) {
+			List<Integer> lines = parsedLines.get(l.getFile());
+			if(!lines.contains(i)) {
+				amountOfLines++;
+				lines.add(i);
+			} 
+		}
+		return amountOfLines;
+	}
+
+	public void reportClones(List<Sequence> clones) {
+		parsedLines.clear();
+		for(Sequence clone : clones)
+			reportClone(clone);
+	}
+
+	private void reportClone(Sequence clone) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/main/java/com/simonbaars/clonerefactor/metrics/Metrics.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/Metrics.java
@@ -1,5 +1,13 @@
 package com.simonbaars.clonerefactor.metrics;
 
 public class Metrics {
-
+	public int totalAmountOfLines = 0;
+	public int totalAmountOfNodes = 0;
+	public int totalAmountOfTokens = 0;
+	
+	public int amountOfLinesCloned = 0;
+	public int amountOfNodesCloned = 0;
+	public int amountOfTokensCloned = 0;
+	
+	
 }
