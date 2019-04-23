c19bdc1e666850d69dea09dedeba80776d204518
Simon Baars <simon.mailadres@gmail.com>
2019-04-23 12:15:48 +0200

    Interface clone project

---
+++ src/test/resources/InterfaceClone/InterfaceClone1.java
@@ -0,0 +1,10 @@
+public interface InterfaceClone1 {
+
+	public void clone1();
+	public void clone2();
+	public void clone3();
+	public void clone4();
+	public void clone5();
+	public void clone6();
+	public void clone7();
+}
