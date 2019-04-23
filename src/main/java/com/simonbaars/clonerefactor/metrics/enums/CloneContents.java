6003e940f8f7c3e855abf5fc8e526151100ec67d
Simon Baars <simon.mailadres@gmail.com>
2019-04-23 11:52:40 +0200

    Finished up the CloneContents

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneContents.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneContents.java
@@ -1,13 +1,14 @@
 package com.simonbaars.clonerefactor.metrics.enums;
 
-import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.FULLMETHOD;
-import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.PARTIALMETHOD;
-import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.SEVERALMETHODS;
+import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.*;
 
 import java.util.List;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.EnumConstantDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.simonbaars.clonerefactor.ast.NodeParser;
@@ -20,18 +21,21 @@ public class CloneContents implements MetricEnum<ContentsType> {
 		PARTIALMETHOD, 
 		SEVERALMETHODS, 
 		ONLYFIELDS, 
-		INCLUDESFIELDS, 
 		FULLCLASS, 
+		FULLINTERFACE,
+		FULLENUM,
 		HASCLASSDECLARATION, 
 		HASINTERFACEDECLARATION, 
 		HASENUMDECLARATION, 
-		HASENUMFIELDS;
+		HASENUMFIELDS,
+		INCLUDESFIELDS,
+		MIXED;
 	}
 
 	@Override
 	public ContentsType get(Sequence sequence) {
 		List<Node> nodes = sequence.getAny().getContents().getNodes();
-		if(nodes.get(0) instanceof MethodDeclaration && nodes.get(nodes.size()-1) == getLastStatement((MethodDeclaration)nodes.get(0))) {
+		if(nodes.get(0) instanceof MethodDeclaration && nodes.get(nodes.size()-1) == getLastStatement(nodes.get(0))) {
 			return FULLMETHOD;
 		} else if(getMethod(nodes.get(0)) == getMethod(nodes.get(nodes.size()-1))) {
 			return PARTIALMETHOD;
@@ -39,12 +43,28 @@ public class CloneContents implements MetricEnum<ContentsType> {
 			return SEVERALMETHODS;
 		} else if(nodes.stream().allMatch(e -> getMethod(e)== null && e instanceof FieldDeclaration)) {
 			return ONLYFIELDS;
+		} else if(nodes.get(0) instanceof ClassOrInterfaceDeclaration && !((ClassOrInterfaceDeclaration)nodes.get(0)).isInterface() && nodes.get(nodes.size()-1) == getLastStatement(nodes.get(0))) {
+			return FULLCLASS;
+		} else if(nodes.get(0) instanceof ClassOrInterfaceDeclaration && ((ClassOrInterfaceDeclaration)nodes.get(0)).isInterface() && nodes.get(nodes.size()-1) == getLastStatement(nodes.get(0))) {
+			return FULLINTERFACE;
+		} else if(nodes.get(0) instanceof EnumDeclaration && nodes.get(nodes.size()-1) == getLastStatement(nodes.get(0))) {
+			return FULLENUM;
+		} else if(nodes.stream().anyMatch(e -> e instanceof ClassOrInterfaceDeclaration && !((ClassOrInterfaceDeclaration)e).isInterface())) {
+			return HASCLASSDECLARATION;
+		} else if(nodes.stream().anyMatch(e -> e instanceof ClassOrInterfaceDeclaration && ((ClassOrInterfaceDeclaration)e).isInterface())) {
+			return HASINTERFACEDECLARATION;
+		} else if(nodes.stream().anyMatch(e -> e instanceof EnumDeclaration)) {
+			return HASENUMDECLARATION;
+		} else if(nodes.stream().anyMatch(e -> e instanceof EnumConstantDeclaration)) {
+			return HASENUMFIELDS;
+		} else if(nodes.stream().anyMatch(e -> getMethod(e)== null && e instanceof FieldDeclaration)) {
+			return INCLUDESFIELDS;
 		}
-		return null;
+		return MIXED;
 	}
 
-	private Node getLastStatement(MethodDeclaration methodDeclaration) {
-		List<Node> children = methodDeclaration.getChildNodes();
+	private Node getLastStatement(Node n) {
+		List<Node> children = n.getChildNodes();
 		if(children.get(children.size()-1) instanceof BodyDeclaration)
 			children = children.get(children.size()-1).getChildNodes();
 		return children.stream().filter(e -> !NodeParser.isExcluded(e)).reduce((first, second) -> second).get();
