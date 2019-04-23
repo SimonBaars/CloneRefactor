70b8197a9eecea914bb8097f480e050b87882f09
Simon Baars <simon.mailadres@gmail.com>
2019-04-23 13:56:56 +0200

    First half of the contents test

---
+++ src/test/java/com/simonbaars/clonerefactor/CloneContentsTest.java
@@ -0,0 +1,61 @@
+package com.simonbaars.clonerefactor;
+
+import com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType;
+import com.simonbaars.clonerefactor.metrics.enums.CloneLocation.LocationType;
+import com.simonbaars.clonerefactor.model.DetectionResults;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for the node locations.
+ */
+public class CloneContentsTest extends TestCase {
+
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CloneContentsTest( String testName ) {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite( CloneContentsTest.class );
+    }
+    
+    public void testFullMethod() {
+        test("SimpleClone", ContentsType.FULLMETHOD);
+    }
+    
+    public void testPartialMethod() {
+        test("SingleFile", ContentsType.PARTIALMETHOD);
+    }
+    
+    public void testSeveralMethods() {
+        test("SeveralMethodsCloned", ContentsType.SEVERALMETHODS);
+    }
+    
+    public void testMethodLevel1() {
+        test("OnlyFields", ContentsType.ONLYFIELDS);
+    }
+    
+    public void testMethodLevel2() {
+        test("IncludesFields", ContentsType.INCLUDESFIELDS);
+    }
+
+	private void test(String name, ContentsType loc) {
+		DetectionResults r = testProject(name);
+        Assert.assertEquals(loc, r.getMetrics().amountPerContents.keySet().iterator().next());
+	}
+
+	private DetectionResults testProject(String project) {
+		return Main.cloneDetection(CloneContentsTest.class.getClassLoader().getResource(project).getFile());
+	}
+}
---
+++ src/test/resources/IncludesFields/Clone1.java
@@ -0,0 +1,16 @@
+
+public class Clone1 {
+	
+	private String clone1 = "I'm a clone1";
+	private String clone2 = "I'm a clone2";
+	private String clone3 = "I'm a clone3";
+	private String clone4 = "I'm a clone4";
+	private String clone5 = "I'm a clone5";
+	private String clone6 = "I'm a clone6";
+	private String clone7 = "I'm a clone7";
+
+	public static void main(String[] args) {
+		System.out.println("this method is cloned");
+	}
+
+}
