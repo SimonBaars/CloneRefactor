62eec03dc41bd993999d6fee07c7c7b5dec22642
Simon Baars <simon.mailadres@gmail.com>
2019-04-23 11:10:06 +0200

    More CloneContents

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneContents.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneContents.java
@@ -1,9 +1,16 @@
 package com.simonbaars.clonerefactor.metrics.enums;
 
+import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.FULLMETHOD;
+import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.PARTIALMETHOD;
+import static com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType.SEVERALMETHODS;
+
 import java.util.List;
 
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.simonbaars.clonerefactor.ast.NodeParser;
 import com.simonbaars.clonerefactor.metrics.enums.CloneContents.ContentsType;
 import com.simonbaars.clonerefactor.model.Sequence;
 
@@ -25,13 +32,21 @@ public class CloneContents implements MetricEnum<ContentsType> {
 	public ContentsType get(Sequence sequence) {
 		List<Node> nodes = sequence.getAny().getContents().getNodes();
 		if(nodes.get(0) instanceof MethodDeclaration && nodes.get(nodes.size()-1) == getLastStatement((MethodDeclaration)nodes.get(0))) {
-			
+			return FULLMETHOD;
+		} else if(getMethod(nodes.get(0)) == getMethod(nodes.get(nodes.size()-1))) {
+			return PARTIALMETHOD;
+		} else if(nodes.stream().allMatch(e -> getMethod(e)!=null)) {
+			return SEVERALMETHODS;
+		} else if(nodes.stream().allMatch(e -> getMethod(e)== null && e instanceof FieldDeclaration)) {
+			return ONLYFIELDS;
 		}
 		return null;
 	}
 
 	private Node getLastStatement(MethodDeclaration methodDeclaration) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Node> children = methodDeclaration.getChildNodes();
+		if(children.get(children.size()-1) instanceof BodyDeclaration)
+			children = children.get(children.size()-1).getChildNodes();
+		return children.stream().filter(e -> !NodeParser.isExcluded(e)).reduce((first, second) -> second).get();
 	}
 }
