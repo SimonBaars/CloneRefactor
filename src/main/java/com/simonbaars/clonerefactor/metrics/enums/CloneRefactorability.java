1bc2c35a363167137c56be871ce874fd39fec3f8
Simon Baars <simon.mailadres@gmail.com>
2019-04-26 14:52:39 +0200

    Wrote some stuff regarding clone refactorability

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneRefactorability.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneRefactorability.java
@@ -1,17 +1,33 @@
 package com.simonbaars.clonerefactor.metrics.enums;
 
+import java.util.List;
+
+import com.github.javaparser.ast.Node;
 import com.simonbaars.clonerefactor.ast.RequiresNodeOperations;
 import com.simonbaars.clonerefactor.metrics.enums.CloneRefactorability.Refactorability;
+import com.simonbaars.clonerefactor.model.Location;
 import com.simonbaars.clonerefactor.model.Sequence;
 
 public class CloneRefactorability implements MetricEnum<Refactorability>, RequiresNodeOperations {
 	public enum Refactorability{
-		EASY, 
-		HARD
+		CANBEEXTRACTED,
+		NOEXTRACTIONBYCONTENTTYPE,
+		CANNOTBEEXTRACTED
 	}
 
 	@Override
 	public Refactorability get(Sequence sequence) {
-		return Refactorability.EASY;
+		if(new CloneContents().get(sequence)!=CloneContents.ContentsType.PARTIALMETHOD)
+			return Refactorability.NOEXTRACTIONBYCONTENTTYPE;
+		for(Location location : sequence.getSequence()) {
+			for(Node n : location.getContents().getNodes()) {
+				List<Node> children = childrenToParse(n);
+				if(children.stream().anyMatch(e -> !isExcluded(e) && !location.getContents().getNodes().contains(e))) {
+					System.out.println("Child "+children.stream().filter(e -> !isExcluded(e) && !location.getContents().getNodes().contains(e)).findFirst().get()+" not included!!!");
+					return Refactorability.CANNOTBEEXTRACTED;
+				}
+			}
+		}
+		return Refactorability.CANBEEXTRACTED;
 	}
 }
