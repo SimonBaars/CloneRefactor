44a8736dcb5d9eb0fe2c78ae251c859472350819
Simon Baars <simon.mailadres@gmail.com>
2019-04-29 07:29:21 +0200

    A simple config

--- src/main/java/com/simonbaars/clonerefactor/Main.java
+++ src/main/java/com/simonbaars/clonerefactor/Main.java
@@ -11,13 +11,17 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.type.Type;
+import com.github.javaparser.resolution.SymbolResolver;
+import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.utils.SourceRoot;
 import com.simonbaars.clonerefactor.ast.CloneParser;
 import com.simonbaars.clonerefactor.exception.NoJavaFilesFoundException;
-import com.simonbaars.clonerefactor.exception.NoPathEnteredException;
 import com.simonbaars.clonerefactor.model.DetectionResults;
 
 public class Main {
@@ -35,7 +39,10 @@ public class Main {
 	}
 
 	public static DetectionResults cloneDetection(Path path) {
-		SourceRoot sourceRoot = new SourceRoot(path);
+		ParserConfiguration config = new ParserConfiguration()
+				.setLexicalPreservationEnabled(false) //Disabled for now, we'll enable it when we start refactoring.
+				.setStoreTokens(true);
+		SourceRoot sourceRoot = new SourceRoot(path, config);
 		return new CloneParser().parse(sourceRoot.getCompilationUnits());
 	}
 	
