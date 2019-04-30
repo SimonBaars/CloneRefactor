1d6674b74903625b255e05819bb44ab5f38b6465
Simon Baars <simon.mailadres@gmail.com>
2019-04-30 13:36:31 +0200

    Create CompareToken.java

---
+++ src/main/java/com/simonbaars/clonerefactor/compare/CompareToken.java
@@ -0,0 +1,33 @@
+package com.simonbaars.clonerefactor.compare;
+
+import com.github.javaparser.JavaToken;
+
+public class CompareToken implements Compare {
+	
+	private final JavaToken token;
+	
+	public CompareToken(JavaToken token) {
+		super();
+		this.token = token;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return token.equals(((CompareToken)o).token);
+	}
+
+	@Override
+	public boolean isValid() {
+		return true;
+	}
+
+	@Override
+	public int getHashCode() {
+		return token.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "CompareNode [token=" + token + "]";
+	}
+}
