a0fc8832d0004dbce550b619b1a6cf1700d5839f
Simon Baars <simon.mailadres@gmail.com>
2019-05-02 15:45:34 +0200

    Fixed a issue where other exceptions could get thrown from some pesky method

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneRelation.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneRelation.java
@@ -152,7 +152,7 @@ public class CloneRelation implements MetricEnum<RelationType> {
 	private String getFullyQualifiedName(ClassOrInterfaceType t) {
 		try { 
 			return t.resolve().getQualifiedName();
-		} catch (UnsolvedSymbolException | IllegalStateException e) {
+		} catch (Exception e) {
 			return t.getNameAsString();
 		}
 	}
@@ -160,7 +160,7 @@ public class CloneRelation implements MetricEnum<RelationType> {
 	private String getFullyQualifiedName(ClassOrInterfaceDeclaration c2) {
 		try { 
 			return c2.resolve().getQualifiedName();
-		} catch (UnsolvedSymbolException | IllegalStateException e) {
+		} catch (Exception e) {
 			return c2.getNameAsString();
 		}
 	}
