b1f282dc8c8c431790860830760b43b6678768ec
Simon Baars <simon.mailadres@gmail.com>
2019-05-08 12:43:36 +0200

    Update IndexedVector.java

--- src/main/java/com/simonbaars/clonerefactor/datatype/IndexedVector.java
+++ src/main/java/com/simonbaars/clonerefactor/datatype/IndexedVector.java
@@ -1,6 +1,5 @@
 package com.simonbaars.clonerefactor.datatype;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Vector;
 
 public class IndexedVector<E> extends Vector<E> {
@@ -10,18 +9,12 @@ public class IndexedVector<E> extends Vector<E> {
 	 */
 	private static final long serialVersionUID = 9150447915872304886L;
 	
-	public synchronized int getIndex(Class<E> c) {
+	public synchronized int getIndex(E element) {
 		int size = size();
-		try {
-			E el = c.getConstructor(int.class).newInstance(size);
-			add(el);
-			if(indexOf(el)!=size)
-				throw new IllegalStateException("Synchronization error!");
-			return size;
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
-			throw new IllegalStateException(e);
-		}
+		add(element);
+		if(indexOf(element)!=size)
+			throw new IllegalStateException("Synchronization error!");
+		return size;
 	}
 	
 }
