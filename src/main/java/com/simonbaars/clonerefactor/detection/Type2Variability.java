6d7f8dadb39405b1c315d098f6116837ad32ce7f
Simon Baars <simon.mailadres@gmail.com>
2019-05-09 08:11:44 +0200

    Calculate variability with a graph

--- src/main/java/com/simonbaars/clonerefactor/detection/Type2Variability.java
+++ src/main/java/com/simonbaars/clonerefactor/detection/Type2Variability.java
@@ -3,32 +3,46 @@ package com.simonbaars.clonerefactor.detection;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.javaparser.utils.Pair;
 import com.simonbaars.clonerefactor.compare.CloneType;
 import com.simonbaars.clonerefactor.compare.Compare;
-import com.simonbaars.clonerefactor.datatype.CountMap;
 import com.simonbaars.clonerefactor.model.Location;
 import com.simonbaars.clonerefactor.model.Sequence;
 
 public class Type2Variability {
 	public static final int MAX_VARIABILITY = 20;
 	
-	public boolean determineVariability(Sequence s) {
+	public List<Sequence> determineVariability(Sequence s) {
 		List<List<Compare>> literals = createLiteralList(s);
 		int[][] equalityArray = createEqualityArray(literals);
-		CountMap<Integer> cm = new CountMap<>(); 
+		List<List<Integer>> connections = findConnectedSequences(equalityArray);
+		return determineOutput(s, connections);
+	}
+
+	private List<Sequence> determineOutput(Sequence s, List<List<Integer>> connections) {
+		List<Sequence> output = new ArrayList<>();
+		for(List<Integer> connection : connections) {
+			if(connection.size()>1) {
+				Sequence seq = new Sequence();
+				for(Integer i : connection) {
+					seq.add(s.getSequence().get(i));
+				}
+				output.add(seq);
+			}
+		}
+		return output;
+	}
+
+	private List<List<Integer>> findConnectedSequences(int[][] equalityArray) {
+		Graph g = new Graph(equalityArray.length);
 		for(int i = 0; i<equalityArray.length; i++) {
 			for(int j = i+1; j<equalityArray.length; j++) {
 				if(diffPerc(equalityArray[i], equalityArray[j])<=MAX_VARIABILITY) {
-					cm.increment(i);
-					cm.increment(j);
+					g.addEdge(i, j);
 				}
 			}
 		}
-		for(Integer i : cm.keySet()) {
-			
-		}
-		return true;
+		List<List<Integer>> connections = g.connectedComponents();
+		return connections;
 	}
 
 	private int[][] createEqualityArray(List<List<Compare>> literals) {
