a07389fb53b08e9e219b0d85556e0643e45dbbcf
Simon Baars <simon.mailadres@gmail.com>
2019-05-13 17:04:16 +0200

    Created test for type 3 clones

---
+++ src/test/java/com/simonbaars/clonerefactor/Type3Test.java
@@ -0,0 +1,40 @@
+package com.simonbaars.clonerefactor;
+
+import java.nio.file.Paths;
+
+import com.simonbaars.clonerefactor.model.DetectionResults;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for the clone detector.
+ */
+public class Type3Test extends TestCase {    
+
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Type3Test( String testName ) {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(Type3Test.class);
+    }
+    
+    public void testSimpleClone() {
+    	System.out.println("testSimpleClone");
+    	System.out.println(testProject("SimpleClone"));
+    }
+
+	private DetectionResults testProject(String project) {
+		return Main.cloneDetection(Type3Test.class.getClassLoader().getResource("Type3"+project).getFile()).sorted();
+	}
+}
