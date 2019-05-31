b9847386e60e46428a3a354b9dfe2bae33b35724
Simon Baars <simon.mailadres@gmail.com>
2019-05-31 18:44:58 +0200

    Remove (unneeded?) type 2 variability code
    RIP countless hours of work

--- src/main/java/com/simonbaars/clonerefactor/detection/type2/Type2Variability.java
+++ src/main/java/com/simonbaars/clonerefactor/detection/type2/Type2Variability.java
@@ -2,28 +2,21 @@ package com.simonbaars.clonerefactor.detection.type2;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import com.github.javaparser.Range;
-import com.github.javaparser.ast.Node;
 import com.simonbaars.clonerefactor.compare.Compare;
-import com.simonbaars.clonerefactor.datatype.CountMap;
-import com.simonbaars.clonerefactor.datatype.ListMap;
 import com.simonbaars.clonerefactor.detection.interfaces.CalculatesPercentages;
 import com.simonbaars.clonerefactor.detection.interfaces.ChecksThresholds;
-import com.simonbaars.clonerefactor.detection.interfaces.RemovesDuplicates;
 import com.simonbaars.clonerefactor.model.Sequence;
 import com.simonbaars.clonerefactor.model.location.Location;
 import com.simonbaars.clonerefactor.settings.CloneType;
 
-public class Type2Variability implements CalculatesPercentages, ChecksThresholds, RemovesDuplicates {
+public class Type2Variability implements CalculatesPercentages, ChecksThresholds {
 	public List<Sequence> determineVariability(Sequence s) {
 		List<List<Compare>> literals = createLiteralList(s);
 		int[][] equalityArray = createEqualityArray(literals);
@@ -77,77 +70,8 @@ public class Type2Variability implements CalculatesPercentages, ChecksThresholds
 	}
 
 	private List<Sequence> findAllValidSubSequences(Sequence s, List<List<Compare>> literals, int[][] equalityArray) {
-		//List<Sequence> sequences = new ArrayList<>();
 		Map<Integer, int[][]> statementEqualityArrays = findConnectedStatements(s, literals, equalityArray);
-		/*for(int[] relevantLocationIndices : powerset(IntStream.range(0, s.size()).toArray())){
-			if(relevantLocationIndices.length>1)
-				sliceSequence(sequences, s, statementEqualityArrays, relevantLocationIndices);
-			else if (relevantLocationIndices.length == 1) 
-				//sliceSequence(sequences, s, statementEqualityArrays, new int[] {relevantLocationIndices[0], relevantLocationIndices[0]});
-				findInnerClones(sequences, s, statementEqualityArrays, relevantLocationIndices[0]);
-		}*/
 		return findDisplacedClones(s, literals, statementEqualityArrays);
-	}
-	
-	private void findInnerClones(List<Sequence> sequences, Sequence s, Map<Integer, int[][]> statementEqualityArrays, int index) {
-		final ListMap<EqualityArray, Integer> stuffOnLine = new ListMap<>();
-		statementEqualityArrays.keySet().stream().forEach(i -> stuffOnLine.addTo(new EqualityArray(statementEqualityArrays.get(i)[index]), i));
-		final CountMap<Integer> chainList = new CountMap<>();
-		
-		for(Integer i : statementEqualityArrays.keySet()) {
-			Entry<EqualityArray, List<Integer>> clones = stuffOnLine.getEntryForValue(i);
-			ListMap<Integer, Integer> endingChains = new ListMap<>();
-
-			List<Integer> removeChain = new ArrayList<>();
-			chainList.entrySet().stream().forEach(curChain -> {
-				Optional<Integer> chain = clones.getValue().stream().filter(newClone -> newClone == curChain.getKey()+curChain.getValue()).findAny();
-				if(chain.isPresent()) {
-					curChain.setValue(curChain.getValue()+1);
-					//clones.getValue().remove(chain.get());
-				} 
-				if(!chain.isPresent() || i == statementEqualityArrays.size()-1){
-					endingChains.addTo(curChain.getValue(), curChain.getKey());
-					removeChain.add(curChain.getKey());
-				}
-			});
-			removeChain.forEach(chainList::remove);
-			
-			createSequencesOf(sequences, s, endingChains, index);
-			
-			clones.getValue().forEach(chainList::increment);
-			clones.getValue().remove(i);
-		}
-	}
-
-	private void createSequencesOf(List<Sequence> sequences, Sequence s, ListMap<Integer, Integer> endingChains, int index) {
-		for(Integer i : endingChains.keySet().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList())) {
-			List<Integer> e = endingChains.get(i);
-			e.addAll(endingChains.entrySet().stream().filter(f -> f.getKey()<i).flatMap(f -> f.getValue().stream()).collect(Collectors.toList()));
-			if(e.size()>1) {
-				Sequence newSeq = createSequence(s, e, i, index);
-				if(checkThresholds(newSeq) && removeDuplicatesOf(sequences, newSeq)) 
-					sequences.add(newSeq);
-			}
-		}
-	}
-
-	private Sequence createSequence(Sequence s, List<Integer> startIndices, int size, int index) {
-		Location l = s.getSequence().get(index);
-		Sequence newSeq = new Sequence();
-		for(Integer i : startIndices) {
-			Location l2 = new Location(l);
-			newSeq.add(l2);
-			List<Node> myNodes = l2.getContents().getNodes();
-			for(int nodeIndex = myNodes.size()-1; nodeIndex>=0; nodeIndex--)
-				if(nodeIndex<i || nodeIndex>i+size)
-					myNodes.remove(nodeIndex);
-			Range r = new Range(myNodes.get(0).getRange().get().begin, findNodeLocation(getStatementLoc(l2), myNodes.get(myNodes.size()-1)).getRange().end);
-			l2.setRange(r);
-			l2.getContents().setRange(r);
-			l2.getContents().stripToRange();
-			
-		}
-		return newSeq;
 	}
 
 	// https://stackoverflow.com/questions/40201309/best-way-to-get-a-power-set-of-an-array
@@ -163,77 +87,10 @@ public class Type2Variability implements CalculatesPercentages, ChecksThresholds
 		return result;
 	}
 	
-	private void sliceSequence(List<Sequence> sequences, Sequence s, Map<Integer, int[][]> statementEqualityArrays, int[] relevantLocationIndices) {
-		List<WeightedPercentage> calcPercentages = getWeightedPercentages(s, statementEqualityArrays, relevantLocationIndices);
-		List<WeightedPercentage> percentagesList = new ArrayList<>();
-		for(int i = 0; i<calcPercentages.size(); i++) {
-			percentagesList.add(calcPercentages.get(i));
-			checkCloneValidity(sequences, s, relevantLocationIndices, calcPercentages, percentagesList, i);
-		}
-	}
-
-	private void checkCloneValidity(List<Sequence> sequences, Sequence s, int[] relevantLocationIndices,
-			List<WeightedPercentage> calcPercentages, List<WeightedPercentage> percentagesList, int i) {
-		boolean notValidRegardingVariability = !checkType2VariabilityThreshold(calcAvg(percentagesList));
-		if((notValidRegardingVariability && !canFixIt(calcPercentages, percentagesList, i)) || i+1 == calcPercentages.size()) {
-			if(percentagesList.size()>1) {
-				if(notValidRegardingVariability) percentagesList.remove(percentagesList.size()-1);
-				Sequence newSeq = createSequence(s, calcPercentages.indexOf(percentagesList.get(0)), calcPercentages.indexOf(percentagesList.get(percentagesList.size()-1)), relevantLocationIndices);
-				if(checkThresholds(newSeq) && removeDuplicatesOf(sequences, newSeq))
-					sequences.add(newSeq);
-			}
-			percentagesList.clear();
-		}
-	}
-
-	private Sequence createSequence(Sequence s, int from, int to, int[] relevantIndices) {
-		Sequence newSeq = new Sequence();
-		for(int locationIndex : relevantIndices) {
-			Location l = s.getSequence().get(locationIndex);
-			Location l2 = new Location(l);
-			newSeq.add(l2);
-			List<Node> myNodes = l2.getContents().getNodes();
-			for(int nodeIndex = myNodes.size()-1; nodeIndex>=0; nodeIndex--)
-				if(nodeIndex<from || nodeIndex>to)
-					myNodes.remove(nodeIndex);
-			Range r = new Range(myNodes.get(0).getRange().get().begin, findNodeLocation(getStatementLoc(l2), myNodes.get(myNodes.size()-1)).getRange().end);
-			l2.setRange(r);
-			l2.getContents().setRange(r);
-			l2.getContents().stripToRange();
-		}
-		return newSeq;
-	}
-	
 	public Location getStatementLoc(Location l) {
 		if(l.getNext() != null)
 			return l.getNext().getPrev();
 		return l.getPrev().getNext();
-	}
-	
-	private Location findNodeLocation(Location l, Node n) {
-		if(l.getContents().getNodes().get(0) == n)
-			return l;
-		return findNodeLocation(l.getNext(), n);
-	}
-
-	private List<WeightedPercentage> getWeightedPercentages(Sequence s, Map<Integer, int[][]> statementEqualityArrays, int[] relevantLocationIndices) {
-		List<WeightedPercentage> calcPercentages = new ArrayList<>();
-		for(int currNodeIndex = 0; currNodeIndex<s.getAny().getContents().getNodes().size(); currNodeIndex++) {
-			int[][] equality = statementEqualityArrays.get(currNodeIndex);
-			calcPercentages.add(new WeightedPercentage(diffPerc(equality, relevantLocationIndices), equality[0].length));
-		}
-		return calcPercentages;
-	}
-
-	private boolean canFixIt(List<WeightedPercentage> calcPercentages, List<WeightedPercentage> percentagesList, int i) {
-		percentagesList = new ArrayList<>(percentagesList);
-		for(i++; i<calcPercentages.size(); i++) {
-			percentagesList.add(calcPercentages.get(i));
-			if(checkType2VariabilityThreshold(calcAvg(percentagesList))){
-				return true;
-			}
-		}
-		return false;
 	}
 
 	//Creates a per statement equality array.
