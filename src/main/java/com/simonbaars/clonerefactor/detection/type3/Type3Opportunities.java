ffbfdb6161036b27de6803ad41429e22229a30a6
Simon Baars <simon.mailadres@gmail.com>
2019-06-05 11:08:35 +0200

    Recoded Type 3 Opportunities

--- src/main/java/com/simonbaars/clonerefactor/detection/type3/Type3Opportunities.java
+++ src/main/java/com/simonbaars/clonerefactor/detection/type3/Type3Opportunities.java
@@ -1,8 +1,10 @@
 package com.simonbaars.clonerefactor.detection.type3;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
+import com.github.javaparser.ast.Node;
 import com.simonbaars.clonerefactor.datatype.ListMap;
 import com.simonbaars.clonerefactor.detection.interfaces.CalculatesPercentages;
 import com.simonbaars.clonerefactor.model.Sequence;
@@ -13,16 +15,21 @@ public class Type3Opportunities implements Type3Calculation, CalculatesPercentag
 	private ListMap<Integer, FileLocations> opportunities = new ListMap<>();
 	
 	public void determineType3Opportunities(List<Sequence> clones) {
-		for(int i = 0; i<clones.size(); i++) {
+		outerloop: for(int i = 0; i<clones.size(); i++) {
+			System.out.println(i+"/"+clones.size());
 			FileLocations fl = new FileLocations(clones.get(i));
 			if(opportunities.containsKey(fl.hashCode())) {
 				 List<FileLocations> otherFls = opportunities.get(fl.hashCode());
-				 for(FileLocations loc : otherFls) {
+				 for(int j = 0; j<otherFls.size(); j++) {
+					 FileLocations loc = otherFls.get(j);
 					 if(isType3(fl, loc)) {
+						 System.out.println("Merge "+fl+" and "+loc);
 						 clones.remove(fl.getSeq());
 						 clones.remove(loc.getSeq());
+						 otherFls.remove(loc);
 						 clones.add(merge(fl.getLocs(), loc.getLocs()));
 						 i--;
+						 continue outerloop;
 					 }
 				 }
 			} 
@@ -32,29 +39,37 @@ public class Type3Opportunities implements Type3Calculation, CalculatesPercentag
 	
 	private Sequence merge(List<Location> fl1, List<Location> fl2) {
 		Sequence seq = new Sequence();
-		for(int i = 0; i<fl1.size(); i++) {
-			seq.add(new Type3Location(fl1.get(i), fl2.get(i)));
-		}
+		for(int i = 0; i<fl1.size(); i++)
+			seq.add(calculateDiff(fl1.get(i), fl2.get(i)) == 0 ? fl1.get(i).mergeWith(fl2.get(i)) : new Type3Location(fl1.get(i), fl2.get(i)));
 		return seq;
 	}
 
 	public boolean isType3(FileLocations fl1, FileLocations fl2) {
-		return IntStream.range(0, fl1.getLocs().size()).anyMatch(i -> {
+		return IntStream.range(0, fl1.getLocs().size()).allMatch(i -> {
 			Location l1 = fl1.getLocs().get(i);
 			Location l2 = fl2.getLocs().get(i);
-			if(!Settings.get().isUseLiteratureTypeDefinitions() && !l1.getContents().getNodes().get(l1.getContents().getNodes().size()-1).getParentNode().equals(l2.getContents().getNodes().get(0).getParentNode()))
+			System.out.println(l1 + " vs "+l2);
+			if(l1.getFile() != l2.getFile())
 				return false;
-			if(l1.getRange().isBefore(l2.getRange().begin)) {
+			System.out.println("Same file ");
+			//System.out.println(l1.getContents().getNodes().get(l1.getContents().getNodes().size()-1).getParentNode()+" vs "+l2.getContents().getNodes().get(0).getParentNode());
+			if(!Settings.get().isUseLiteratureTypeDefinitions() && !parentsEqual(l1.getContents().getNodes().get(l1.getContents().getNodes().size()-1).getParentNode(), l2.getContents().getNodes().get(0).getParentNode()))
+				return false;
+			System.out.println("Same parent "+checkType3Threshold(l1, l2));
+			if(l1.getRange().begin.isBefore(l2.getRange().begin)) {
 				return checkType3Threshold(l1, l2);
 			} else return checkType3Threshold(l2, l1);
 		});
+	}
+	
+	public boolean parentsEqual(Optional<Node> n1, Optional<Node> n2) {
+		return n1.isPresent() && n2.isPresent() ? n1.get() == n2.get() : !n1.isPresent() && !n2.isPresent();
 	}
 
 	private boolean checkType3Threshold(Location l1, Location l2) {
 		int combinedSize = l1.getAmountOfNodes() + l2.getAmountOfNodes();
 		int diff = calculateDiff(l1, l2);
-		if(diff == 0)
-			return false;
+		System.out.println("res"+calcPercentage(diff, combinedSize));
 		return calcPercentage(diff, combinedSize) <= Settings.get().getType3GapSize();
 	}
 }
