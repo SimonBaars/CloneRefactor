a71316dbb57e4e4b7241ed7536c5c8d2ab61dc28
Simon Baars <simon.mailadres@gmail.com>
2019-06-06 12:56:32 +0200

    Disabled this check again

--- src/main/java/com/simonbaars/clonerefactor/model/Sequence.java
+++ src/main/java/com/simonbaars/clonerefactor/model/Sequence.java
@@ -122,8 +122,8 @@ public class Sequence implements Comparable<Sequence> {
 			throw new IllegalStateException("Not enough locations for "+this);
 		if(locations.stream().map(e -> e.getContents().getNodes().size()).distinct().count()>1)
 			throw new IllegalStateException("Unequal location node sizes for "+this);
-		if(locations.stream().map(e -> e.getContents().getCompare().size()).distinct().count()>1)
-			throw new IllegalStateException("Unequal location compare sizes for "+this);
+		//if(locations.stream().map(e -> e.getContents().getCompare().size()).distinct().count()>1)
+		//	throw new IllegalStateException("Unequal location compare sizes for "+this);
 		return this;
 	}
 }
