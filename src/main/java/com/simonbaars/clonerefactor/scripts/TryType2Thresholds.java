8f33cfe5ad2dbe9fe4668112c82e85fde3385578
Simon Baars <simon.mailadres@gmail.com>
2019-06-06 10:31:16 +0200

    Type 3 thresholds

---
+++ src/main/java/com/simonbaars/clonerefactor/scripts/TryType2Thresholds.java
@@ -0,0 +1,26 @@
+package com.simonbaars.clonerefactor.scripts;
+
+import com.simonbaars.clonerefactor.metrics.Metrics;
+import com.simonbaars.clonerefactor.scripts.model.MetricsTables;
+import com.simonbaars.clonerefactor.settings.CloneType;
+import com.simonbaars.clonerefactor.settings.Settings;
+
+public class TryType2Thresholds implements Runnable {
+	public static void main(String[] args) {
+		new TryType2Thresholds().run();
+	}
+
+	@Override
+	public void run() {
+		MetricsTables metricsTables = new MetricsTables();
+		System.out.println("Try Threshold Percentages");
+		Settings.get().setCloneType(CloneType.TYPE2);
+		for(int i = 0; i<=100; i++) {
+			Settings.get().setType2VariabilityPercentage(i);
+			Metrics metrics = new RunOnCorpus().startCorpusCloneDetection();
+			if(metrics != null)
+				metricsTables.collectMetrics(Integer.toString(i), metrics);
+			metricsTables.writeTables();
+		}
+	}
+}
---
+++ src/main/java/com/simonbaars/clonerefactor/scripts/TryType3Thresholds.java
@@ -0,0 +1,26 @@
+package com.simonbaars.clonerefactor.scripts;
+
+import com.simonbaars.clonerefactor.metrics.Metrics;
+import com.simonbaars.clonerefactor.scripts.model.MetricsTables;
+import com.simonbaars.clonerefactor.settings.CloneType;
+import com.simonbaars.clonerefactor.settings.Settings;
+
+public class TryType3Thresholds implements Runnable {
+	public static void main(String[] args) {
+		new TryType3Thresholds().run();
+	}
+
+	@Override
+	public void run() {
+		MetricsTables metricsTables = new MetricsTables();
+		System.out.println("Try Type 3 Thresholds");
+		Settings.get().setCloneType(CloneType.TYPE3);
+		for(int i = 1; i<=100; i++) {
+			Settings.get().setType3GapSize(i*2);
+			Metrics metrics = new RunOnCorpus().startCorpusCloneDetection();
+			if(metrics != null)
+				metricsTables.collectMetrics(Integer.toString(i*2), metrics);
+			metricsTables.writeTables();
+		}
+	}
+}
