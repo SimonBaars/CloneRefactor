8b1209d60e95083367874179f1bd88cd8e0c5eeb
Simon Baars <simon.mailadres@gmail.com>
2019-07-03 08:48:27 +0200

    Thinking this through

--- src/main/java/com/simonbaars/clonerefactor/refactoring/ExtractMethodFromSequence.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/ExtractMethodFromSequence.java
@@ -1,9 +1,14 @@
 package com.simonbaars.clonerefactor.refactoring;
 
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.Statement;
+import com.simonbaars.clonerefactor.metrics.enums.CloneRelation.RelationType;
 import com.simonbaars.clonerefactor.model.Sequence;
 
 public class ExtractMethodFromSequence {
 	public void tryToExtractMethod(Sequence s) {
-		
+		RelationType relation = s.getRelationType();
+		MethodDeclaration decl = new MethodDeclaration();
+		s.getAny().getContents().getNodes().forEach(node -> decl.getBody().get().addStatement((Statement)node));
 	}
 }
