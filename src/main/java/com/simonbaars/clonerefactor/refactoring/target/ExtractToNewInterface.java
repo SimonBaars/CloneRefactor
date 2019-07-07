8455980f3ad16fc587cfea8ba14df2cfbd3d84b5
Simon <simon.mailadres@gmail.com>
2019-07-07 09:33:19 +0200

    Add interface to class

--- src/main/java/com/simonbaars/clonerefactor/refactoring/target/ExtractToNewInterface.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/target/ExtractToNewInterface.java
@@ -1,14 +1,20 @@
 package com.simonbaars.clonerefactor.refactoring.target;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
+import com.simonbaars.clonerefactor.model.Sequence;
 
 public class ExtractToNewInterface extends ExtractToClassOrInterface {
 	private static int x = 0;
 	
-	public ExtractToNewInterface() {
+	public ExtractToNewInterface(Sequence s) {
 		super(new ClassOrInterfaceDeclaration(new NodeList<>(), new NodeList<>(), true, new SimpleName("GeneratedInterface"+(x++)), new NodeList<>(), new NodeList<>(), new NodeList<>(), new NodeList<>()));
+		Set<ClassOrInterfaceDeclaration> classOrInterface = s.getLocations().stream().map(l -> getClass(l.getContents().getNodes().get(0))).collect(Collectors.toSet());
+		classOrInterface.forEach(e -> e.addImplementedType(new JavaParser().parseClassOrInterfaceType(getClassOrInterface().getNameAsString()).getResult().get()));
 	}
-
 }
