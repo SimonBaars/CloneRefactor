5a8147680569c880e90cc6e78f2e82de39db00c4
Simon <simon.mailadres@gmail.com>
2019-07-09 10:34:33 +0200

    Only create interface if it does not yet exist

--- src/main/java/com/simonbaars/clonerefactor/refactoring/target/ExtractToNewInterface.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/target/ExtractToNewInterface.java
@@ -7,6 +7,7 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.simonbaars.clonerefactor.model.Sequence;
 
 public class ExtractToNewInterface extends ExtractToClassOrInterface {
@@ -15,6 +16,7 @@ public class ExtractToNewInterface extends ExtractToClassOrInterface {
 	public ExtractToNewInterface(Sequence s) {
 		super(new ClassOrInterfaceDeclaration(new NodeList<>(), new NodeList<>(), true, new SimpleName("GeneratedInterface"+(x++)), new NodeList<>(), new NodeList<>(), new NodeList<>(), new NodeList<>()));
 		Set<ClassOrInterfaceDeclaration> classOrInterface = s.getLocations().stream().map(l -> getClass(l.getContents().getNodes().get(0))).collect(Collectors.toSet());
-		classOrInterface.forEach(e -> e.addImplementedType(new JavaParser().parseClassOrInterfaceType(getClassOrInterface().getNameAsString()).getResult().get()));
+		ClassOrInterfaceType implementedType = new JavaParser().parseClassOrInterfaceType(getClassOrInterface().getNameAsString()).getResult().get();
+		classOrInterface.stream().filter(c -> c.getImplementedTypes().stream().noneMatch(t -> t.getNameAsString().equals(implementedType.getNameAsString()))).forEach(c -> c.addImplementedType(implementedType));
 	}
 }
