88bb0ccfaf97fb9dd68161bbba12460cf6e0b02d
Simon <simon.mailadres@gmail.com>
2019-07-11 13:08:43 +0200

    Change comment

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneRefactorability.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/CloneRefactorability.java
@@ -27,7 +27,7 @@ public class CloneRefactorability implements MetricEnum<Refactorability>, Requir
 		NOEXTRACTIONBYCONTENTTYPE, //When the clone is not a partial method
 		PARTIALBLOCK, //When the clone spans part of a block (TODO: can we make the clone smaller to not make it a partial block, or should we turn it into a type 3 clone?)
 		COMPLEXCONTROLFLOW, //When the clone spans break, continue or return statements. However, exceptions apply:
-							// - All flows end in return
+							// - All flows end in return (however not fully implemented)
 							// - The for loop that is being `continue` or `break` is included
 						
 	}
