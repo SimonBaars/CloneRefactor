8efd2b6b5d3f696a74ba248d15f705dd11050e68
Simon <simon.mailadres@gmail.com>
2019-07-13 20:02:03 +0200

    Set relation if not yet determined

--- src/main/java/com/simonbaars/clonerefactor/metrics/model/Relation.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/model/Relation.java
@@ -7,8 +7,8 @@ import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.simonbaars.clonerefactor.metrics.context.CloneRelation.RelationType;
 
 public class Relation {
-	private final RelationType type;
-	private final ClassOrInterfaceDeclaration intersectingClass;
+	private RelationType type;
+	private ClassOrInterfaceDeclaration intersectingClass;
 	
 	public Relation(RelationType type, ClassOrInterfaceDeclaration intersectingClass) {
 		super();
@@ -24,10 +24,13 @@ public class Relation {
 		return intersectingClass;
 	}
 	
-	public Relation testRelation(RelationType type, /*BiFunction<ComparingClasses, Map<String, ClassOrInterfaceDeclaration>, Optional<ClassOrInterfaceDeclaration>> fetchRelation*/ Supplier<Optional<ClassOrInterfaceDeclaration>> fetchRelation) {
-		Optional<ClassOrInterfaceDeclaration> result;
-		if(type != null || !(result = fetchRelation.get()).isPresent())
-			return this;
-		return new Relation(type, result.get());
+	public void setRelationIfNotYetDetermined(RelationType type, /*BiFunction<ComparingClasses, Map<String, ClassOrInterfaceDeclaration>, Optional<ClassOrInterfaceDeclaration>> fetchRelation*/ Supplier<Optional<ClassOrInterfaceDeclaration>> fetchRelation) {
+		if(type != null){
+			Optional<ClassOrInterfaceDeclaration> result = fetchRelation.get();
+			if(result.isPresent()) {
+				this.type = type;
+				this.intersectingClass = result.get();
+			}
+		}
 	}
 }
