5a91f6ec7c00f4ea823e18ac284ac1cd6ad6dfb9
Simon <simon.mailadres@gmail.com>
2019-07-13 14:42:13 +0200

    Pass classes

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/SeekClassHierarchy.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/SeekClassHierarchy.java
@@ -2,18 +2,24 @@ package com.simonbaars.clonerefactor.metrics.enums.relation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.simonbaars.clonerefactor.metrics.model.ComparingClasses;
 
 public interface SeekClassHierarchy extends ParsesSuperclasses {
-	public default boolean inSameHierarchy(ComparingClasses cc) {
-		List<String> classesInHierarchy = new ArrayList<>();
-		collectSuperclasses(cc.getClassOne(), classesInHierarchy);
-		return collectSuperclasses(cc.getClassTwo(), classesInHierarchy);
+	public default boolean inSameHierarchy(Map<String, ClassOrInterfaceDeclaration> classes, ComparingClasses cc) {
+		return sameHierarchy(classes, cc).isPresent();
 	}
 	
-	public default boolean collectSuperclasses(ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy) {
-		return collectSuperclasses(classDecl, classesInHierarchy, classDecl::getExtendedTypes, this::collectSuperclasses, false);
+	public default Optional<ClassOrInterfaceDeclaration> sameHierarchy(Map<String, ClassOrInterfaceDeclaration> classes, ComparingClasses cc) {
+		List<String> classesInHierarchy = new ArrayList<>();
+		collectSuperclasses(classes, cc.getClassOne(), classesInHierarchy);
+		return collectSuperclasses(classes, cc.getClassTwo(), classesInHierarchy);
+	}
+	
+	public default Optional<ClassOrInterfaceDeclaration> collectSuperclasses(Map<String, ClassOrInterfaceDeclaration> classes, ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy) {
+		return collectSuperclasses(classes, classDecl, classesInHierarchy, classDecl::getExtendedTypes, this::collectSuperclasses, false);
 	}
 }
--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/SeekInterfaceHierarchy.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/SeekInterfaceHierarchy.java
@@ -3,21 +3,25 @@ package com.simonbaars.clonerefactor.metrics.enums.relation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.simonbaars.clonerefactor.metrics.model.ComparingClasses;
 
 public interface SeekInterfaceHierarchy extends ParsesSuperclasses {
 	public default boolean haveSameInterface(Map<String, ClassOrInterfaceDeclaration> classes, ComparingClasses cc) {
+		return sameInterface(classes, cc).isPresent();
+	}
+	
+	public default Optional<ClassOrInterfaceDeclaration> sameInterface(Map<String, ClassOrInterfaceDeclaration> classes, ComparingClasses cc) {
 		List<String> classesInHierarchy = new ArrayList<>();
 		collectInterfaces(classes, cc.getClassOne(), classesInHierarchy);
 		return collectInterfaces(classes, cc.getClassTwo(), classesInHierarchy);
-		cc.getClassOne().resolve()
 	}
 	
-	public default boolean collectInterfaces(Map<String, ClassOrInterfaceDeclaration> classes, ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy) {
-		if(collectSuperclasses(classes, classDecl, classesInHierarchy, classDecl::getExtendedTypes, this::collectInterfaces, true))
-			return true;
+	public default Optional<ClassOrInterfaceDeclaration> collectInterfaces(Map<String, ClassOrInterfaceDeclaration> classes, ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy) {
+		Optional<ClassOrInterfaceDeclaration> result = collectSuperclasses(classes, classDecl, classesInHierarchy, classDecl::getExtendedTypes, this::collectInterfaces, true);
+		if(result.isPresent()) return result;
 		return collectSuperclasses(classes, classDecl, classesInHierarchy, classDecl::getImplementedTypes, this::collectInterfaces, true);
 	}
 }
