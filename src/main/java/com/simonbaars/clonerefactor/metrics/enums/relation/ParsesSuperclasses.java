ed6f75bacf08f3ceff8a6f6634c3d7c8b2fae1c7
Simon <simon.mailadres@gmail.com>
2019-07-13 13:49:27 +0200

    Okay
    We'll refactor this later

--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/ParsesSuperclasses.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/ParsesSuperclasses.java
@@ -1,16 +1,16 @@
 package com.simonbaars.clonerefactor.metrics.enums.relation;
 
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.google.common.base.Optional;
 
 public interface ParsesSuperclasses extends ResolvesFullyQualifiedIdentifiers {
-	public default Optional<ClassOrInterfaceDeclaration> collectSuperclasses(ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy, Supplier<NodeList<ClassOrInterfaceType>> getTypes, BiFunction<ClassOrInterfaceDeclaration, List<String>, Boolean> recurse, boolean isInterface) {
+	public default Optional<ClassOrInterfaceDeclaration> collectSuperclasses(Map<String, ClassOrInterfaceDeclaration> classes, ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy, Supplier<NodeList<ClassOrInterfaceType>> getTypes, TriFunction<Map<String, ClassOrInterfaceDeclaration>, ClassOrInterfaceDeclaration, List<String>, Optional<ClassOrInterfaceDeclaration>> recurse, boolean isInterface) {
 		String className = getFullyQualifiedName(classDecl);
 		if(classesInHierarchy.contains(className) && classDecl.isInterface() == isInterface)
 			return Optional.of(classDecl);
@@ -19,9 +19,9 @@ public interface ParsesSuperclasses extends ResolvesFullyQualifiedIdentifiers {
 			String fullyQualifiedName = getFullyQualifiedName(type);
 			if(classes.containsKey(fullyQualifiedName)) {
 				ClassOrInterfaceDeclaration superClass = classes.get(fullyQualifiedName);
-				return recurse.apply(superClass, classesInHierarchy);
+				return recurse.apply(classes, superClass, classesInHierarchy);
 			}
 		}
-		return false;
+		return Optional.empty();
 	}
 }
--- src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/SeekInterfaceHierarchy.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/enums/relation/SeekInterfaceHierarchy.java
@@ -2,20 +2,22 @@ package com.simonbaars.clonerefactor.metrics.enums.relation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import com.simonbaars.clonerefactor.metrics.enums.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.simonbaars.clonerefactor.metrics.model.ComparingClasses;
 
 public interface SeekInterfaceHierarchy extends ParsesSuperclasses {
-	private boolean haveSameInterface(ComparingClasses cc) {
+	public default boolean haveSameInterface(Map<String, ClassOrInterfaceDeclaration> classes, ComparingClasses cc) {
 		List<String> classesInHierarchy = new ArrayList<>();
-		collectInterfaces(cc.getClassOne(), classesInHierarchy);
-		return collectInterfaces(cc.getClassTwo(), classesInHierarchy);
+		collectInterfaces(classes, cc.getClassOne(), classesInHierarchy);
+		return collectInterfaces(classes, cc.getClassTwo(), classesInHierarchy);
+		cc.getClassOne().resolve()
 	}
 	
-	private boolean collectInterfaces(ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy) {
-		if(collectSuperclasses(classDecl, classesInHierarchy, classDecl::getExtendedTypes, this::collectInterfaces, true))
+	public default boolean collectInterfaces(Map<String, ClassOrInterfaceDeclaration> classes, ClassOrInterfaceDeclaration classDecl, List<String> classesInHierarchy) {
+		if(collectSuperclasses(classes, classDecl, classesInHierarchy, classDecl::getExtendedTypes, this::collectInterfaces, true))
 			return true;
-		return collectSuperclasses(classDecl, classesInHierarchy, classDecl::getImplementedTypes, this::collectInterfaces, true);
+		return collectSuperclasses(classes, classDecl, classesInHierarchy, classDecl::getImplementedTypes, this::collectInterfaces, true);
 	}
 }
