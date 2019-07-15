a24391b6716aa9be244f60d0e65a96b691bc5545
Simon Baars <simon.mailadres@gmail.com>
2019-07-15 11:58:58 +0200

    Added nosuperclass relation

--- src/main/java/com/simonbaars/clonerefactor/metrics/context/analyze/CloneRelation.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/context/analyze/CloneRelation.java
@@ -29,14 +29,15 @@ import com.simonbaars.clonerefactor.model.Sequence;
 
 public class CloneRelation implements MetricEnum<Relation>, SeekClassHierarchy, SeekInterfaceHierarchy { 
 	public enum RelationType { //Please note that the order of these enum constants matters
-		SAMEMETHOD, // Refactor to same class
-		SAMECLASS, // Refactor to same class
-		SUPERCLASS, // Refactor to topmost class
-		ANCESTOR, // Refactor to common parent class
-		SIBLING, // Refactor to common parent class
-		FIRSTCOUSIN, // Refactor to common parent class
-		COMMONHIERARCHY, // Refactor to common parent class
+		SAMEMETHOD, // Refactor to same class as a private method
+		SAMECLASS, // Refactor to same class as a private method
+		SUPERCLASS, // Refactor to topmost class as a protected method
+		ANCESTOR, // Refactor to common parent class as a protected method
+		SIBLING, // Refactor to common parent class as a protected method
+		FIRSTCOUSIN, // Refactor to common parent class as a protected method
+		COMMONHIERARCHY, // Refactor to common parent class as a protected method
 		SAMEINTERFACE, // Refactor common interface as an default method
+		NOSUPERCLASS, // Refactor to newly created abstract class as a protected method
 		EXTERNALSUPERCLASS, // Refactor to newly created interface as a default method
 		UNRELATED // Refactor to newly created interface as a default method
 	}
