23d0472a8f9e20c74be0de3b038a61beb6fb4669
Simon Baars <simon.mailadres@gmail.com>
2019-07-16 10:58:52 +0200

    Better relation naming

--- src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/RelationType.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/RelationType.java
@@ -1,14 +1,26 @@
 package com.simonbaars.clonerefactor.metrics.context.enums;
 public enum RelationType { //Please note that the order of these enum constants matters
-		SAMEMETHOD, // Refactor to same class as a private method
-		SAMECLASS, // Refactor to same class as a private method
-		SUPERCLASS, // Refactor to topmost class as a protected method
-		ANCESTOR, // Refactor to common parent class as a protected method
-		SIBLING, // Refactor to common parent class as a protected method
-		FIRSTCOUSIN, // Refactor to common parent class as a protected method
-		COMMONHIERARCHY, // Refactor to common parent class as a protected method
-		SAMEINTERFACE, // Refactor common interface as an default method
-		NODIRECTSUPERCLASS, // Refactor to newly created abstract class as a protected method
-		EXTERNALSUPERCLASS, // Refactor to newly created interface as a default method
-		UNRELATED // Refactor to newly created interface as a default method
+	SAMEMETHOD("Same Method"), // Refactor to same class as a private method
+	SAMECLASS("Same Class"), // Refactor to same class as a private method
+	SUPERCLASS("Superclass"), // Refactor to topmost class as a protected method
+	ANCESTOR("Ancestor"), // Refactor to common parent class as a protected method
+	SIBLING("Sibling"), // Refactor to common parent class as a protected method
+	FIRSTCOUSIN("First Cousin"), // Refactor to common parent class as a protected method
+	COMMONHIERARCHY("Common Hierarchy"), // Refactor to common parent class as a protected method
+	SAMEINTERFACE("Same Interface"), // Refactor common interface as an default method
+	NODIRECTSUPERCLASS("No Direct Superclass"), // Refactor to newly created abstract class as a protected method
+	EXTERNALSUPERCLASS("External Superclass"), // Refactor to newly created interface as a default method
+	UNRELATED("Unrelated") // Refactor to newly created interface as a default method
+	;
+	
+	private final String name;
+	
+	private RelationType(String name) {
+		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+}
