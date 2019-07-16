6234cba68aa6bee834232bf73fb4f6d98b041269
Simon Baars <simon.mailadres@gmail.com>
2019-07-16 19:13:51 +0200

    Removes duplicates refactor

--- src/main/java/com/simonbaars/clonerefactor/detection/interfaces/RemovesDuplicates.java
+++ src/main/java/com/simonbaars/clonerefactor/detection/interfaces/RemovesDuplicates.java
@@ -5,9 +5,9 @@ import java.util.List;
 import com.simonbaars.clonerefactor.model.Sequence;
 
 public interface RemovesDuplicates {
-	public default boolean removeDuplicatesOf(List<Sequence> clones, Sequence l) {
-		clones.removeIf(e -> isSubset(e, l));
-		return clones.stream().noneMatch(e -> isSubset(l, e));
+	public default boolean removeDuplicatesOf(List<Sequence> clones, Sequence newClone) {
+		clones.removeIf(existingClone -> isSubset(existingClone, newClone));
+		return clones.stream().noneMatch(e -> isSubset(newClone, e));
 	}
 	
 	public default boolean isDuplicate(Sequence l) {
@@ -18,7 +18,7 @@ public interface RemovesDuplicates {
 		return newSeq.getLocations().stream().anyMatch(e -> prev.getLocations().stream().anyMatch(f -> e.getRange().contains(f.getRange())));
 	}
 	
-	public default boolean isSubset(Sequence existentClone, Sequence newClone) {
-		return existentClone.getLocations().stream().allMatch(oldLoc -> newClone.getLocations().stream().anyMatch(newLoc -> oldLoc.getFile() == newLoc.getFile() && newLoc.getRange().contains(oldLoc.getRange())));
+	public default boolean isSubset(Sequence existingClone, Sequence newClone) {
+		return existingClone.getLocations().stream().allMatch(oldLoc -> newClone.getLocations().stream().anyMatch(newLoc -> oldLoc.getFile() == newLoc.getFile() && newLoc.getRange().contains(oldLoc.getRange())));
 	}
 }
