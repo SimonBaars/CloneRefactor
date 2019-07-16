1350cbf07a1f409edeaf430035328aca3bb6d2ca
Simon Baars <simon.mailadres@gmail.com>
2019-07-16 11:05:54 +0200

    ContentsType naming

--- src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/ContentsType.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/ContentsType.java
@@ -1,20 +1,31 @@
 package com.simonbaars.clonerefactor.metrics.context.enums;
 
 public enum ContentsType{
-	FULLMETHOD, 
-	PARTIALMETHOD, 
-	SEVERALMETHODS, 
-	FULLCONSTRUCTOR,
-	PARTIALCONSTRUCTOR,
-	ONLYFIELDS, 
-	FULLCLASS, 
-	FULLINTERFACE,
-	FULLENUM,
-	HASCLASSDECLARATION, 
-	HASINTERFACEDECLARATION, 
-	HASENUMDECLARATION, 
-	HASENUMFIELDS,
-	INCLUDESFIELDS,
-	INCLUDESCONSTRUCTOR,
-	OTHER;
+	FULLMETHOD("Full Method"), 
+	PARTIALMETHOD("Partial Method"), 
+	SEVERALMETHODS("Several Methods"), 
+	FULLCONSTRUCTOR("Full Constructor"),
+	PARTIALCONSTRUCTOR("Partial Constructor"),
+	ONLYFIELDS("Only Fields"), 
+	FULLCLASS("Full Class"), 
+	FULLINTERFACE("Full Interface"),
+	FULLENUM("Full Enum"),
+	HASCLASSDECLARATION("Has Class Declaration"), 
+	HASINTERFACEDECLARATION("Has Interface Declaration"), 
+	HASENUMDECLARATION("Has Enum Declaration"), 
+	HASENUMFIELDS("Has Enum Fields"),
+	INCLUDESFIELDS("Includes Fields"),
+	INCLUDESCONSTRUCTOR("Includes Constructor"),
+	OTHER("Other");
+	
+	private final String name;
+	
+	private ContentsType(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 }
