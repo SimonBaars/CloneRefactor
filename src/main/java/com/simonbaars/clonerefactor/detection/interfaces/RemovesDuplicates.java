6d581d7afdf45a85c0d5fa6555c747abf515ef5f
Simon Baars <simon.mailadres@gmail.com>
2019-07-17 08:44:00 +0200

    Removes duplicates refactor

--- src/main/java/com/simonbaars/clonerefactor/detection/interfaces/RemovesDuplicates.java
+++ src/main/java/com/simonbaars/clonerefactor/detection/interfaces/RemovesDuplicates.java
@@ -14,11 +14,11 @@ public interface RemovesDuplicates {
 		return l.getLocations().stream().allMatch(e -> e.isVisited);
 	}
 	
-	public default boolean isPossiblyRedundant(Sequence newSeq, Sequence prev) {
-		return newSeq.getLocations().stream().anyMatch(e -> prev.getLocations().stream().anyMatch(f -> e.getRange().contains(f.getRange())));
+	public default boolean isPossiblyRedundant(Sequence newSeq, Sequence existingSeq) {
+		return newSeq.getLocations().stream().anyMatch(newLoc -> existingSeq.getLocations().stream().anyMatch(existingLoc -> existingLoc.getFile().equals(newLoc.getFile()) && newLoc.getRange().contains(existingLoc.getRange())));
 	}
 	
 	public default boolean isSubset(Sequence existingClone, Sequence newClone) {
-		return existingClone.getLocations().stream().allMatch(oldLoc -> newClone.getLocations().stream().anyMatch(newLoc -> oldLoc.getFile() == newLoc.getFile() && newLoc.getRange().contains(oldLoc.getRange())));
+		return existingClone.getLocations().stream().allMatch(existingLoc -> newClone.getLocations().stream().anyMatch(newLoc -> existingLoc.getFile().equals(newLoc.getFile()) && newLoc.getRange().contains(existingLoc.getRange())));
 	}
 }
