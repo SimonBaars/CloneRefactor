1745c5c6232e68d96dbd0927e58749e42fc0b0dc
Simon <simon.mailadres@gmail.com>
2019-07-26 17:50:19 +0200

    FQS printer

---
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/populate/MethodNamePrinter.java
@@ -0,0 +1,20 @@
+package com.simonbaars.clonerefactor.refactoring.populate;
+
+import java.util.Optional;
+
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
+import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
+
+public class MethodNamePrinter extends VoidVisitorAdapter<Void> implements ResolvesSymbols {
+
+	@Override
+	public void visit(MethodCallExpr md, Void arg) {
+		super.visit(md, arg);
+		Optional<ResolvedMethodDeclaration> mr = resolve(md::resolve);
+		if(mr.isPresent()) {
+			System.out.println(mr.get().getQualifiedSignature());
+		}
+	}
+}
