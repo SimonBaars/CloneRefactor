fda061c49e8882597d7a0a47563204273e7187f2
Simon Baars <simon.mailadres@gmail.com>
2019-07-31 15:34:57 +0200

    Collect all variables + types

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -1,22 +1,22 @@
 package com.simonbaars.clonerefactor.refactoring.visitor;
 
+import java.util.Map;
 import java.util.Optional;
 
-import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
 import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+import com.github.javaparser.resolution.types.ResolvedType;
 import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
 
-public class VariableVisitor extends VoidVisitorAdapter<Void> implements ResolvesSymbols {
+public class VariableVisitor extends VoidVisitorAdapter<Map<NameExpr, ResolvedType>> implements ResolvesSymbols {
 
 	@Override
-	public void visit(NameExpr ne, Void arg) {
+	public void visit(NameExpr ne, Map<NameExpr, ResolvedType> arg) {
 		super.visit(ne, arg);
 		Optional<ResolvedValueDeclaration> mr = resolve(ne::resolve);
 		if(mr.isPresent()) {
-			System.out.println(mr.get().);
+			arg.put(ne, mr.get().getType());
 		}
 	}
 }
