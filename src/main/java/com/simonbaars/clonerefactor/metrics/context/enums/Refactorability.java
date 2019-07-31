b8fb337a55c207fddbeee96f4cf1fe8b336982bb
Simon Baars <simon.mailadres@gmail.com>
2019-07-31 08:50:07 +0200

    Refactor

--- src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/Refactorability.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/Refactorability.java
@@ -1,11 +1,11 @@
 package com.simonbaars.clonerefactor.metrics.context.enums;
 
-public enum Refactorability{
+public enum Refactorability {
 	CANBEEXTRACTED("Can Be Extracted"), //Can be extracted
 	OVERLAPS("Overlap In Clone Class"),
 	NOSTATEMENT("Top-level Node is not a Statement"),
 	NOEXTRACTIONBYCONTENTTYPE("Is Not A Partial Method"), //When the clone is not a partial method
-	PARTIALBLOCK("Partial Block"), //When the clone spans part of a block (TODO: can we make the clone smaller to not make it a partial block, or should we turn it into a type 3 clone?)
+	PARTIALBLOCK("Spans Part of a Block"), //When the clone spans part of a block
 	COMPLEXCONTROLFLOW("Complex Control Flow"), //When the clone spans break, continue or return statements. However, exceptions apply:
 						// - All flows end in return (however not fully implemented)
 						// - The for loop that is being `continue` or `break` is included
