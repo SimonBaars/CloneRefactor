90e4b40627e20b5fb3de3f00d105dbaeaa4ad409
Simon Baars <simon.mailadres@gmail.com>
2019-07-31 13:55:08 +0200

    Non global extracted method

--- src/main/java/com/simonbaars/clonerefactor/refactoring/populate/PopulateThrows.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/populate/PopulateThrows.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.stmt.ThrowStmt;
@@ -17,40 +16,40 @@ import com.github.javaparser.resolution.types.ResolvedReferenceType;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
 import com.simonbaars.clonerefactor.metrics.context.interfaces.RequiresNodeContext;
+import com.simonbaars.clonerefactor.model.Sequence;
 import com.simonbaars.clonerefactor.model.location.Location;
 
-public class PopulateThrows implements RequiresNodeContext, ResolvesSymbols {
+public class PopulateThrows implements PopulatesExtractedMethod, RequiresNodeContext, ResolvesSymbols {
 
-	private MethodDeclaration decl;
-
-	public PopulateThrows(MethodDeclaration decl) {
-		this.decl = decl;
+	@Override
+	public void execute(MethodDeclaration extractedMethod, Sequence sequence) {
+		execute(extractedMethod, sequence.getAny());
 	}
-
-	public void execute(Location loc) {
+	
+	public void execute(MethodDeclaration extractedMethod, Location loc) {
 		List<Node> nodes = loc.getContents().getNodes();
 		for(Node n : nodes) {
 			if(n instanceof ThrowStmt && !hasCatch(nodes, (ThrowStmt)n)) {
-				addThrowsToNodeList(n);
+				addThrowsToNodeList(extractedMethod, n);
 			}
 		}
 	}
 
-	private void addThrowsToNodeList(Node n) {
+	private void addThrowsToNodeList(MethodDeclaration extractedMethod, Node n) {
 		Expression expr = ((ThrowStmt)n).getExpression();
 		Optional<ResolvedType> rt = resolve(() -> expr.calculateResolvedType());
 		if(rt.isPresent() && rt.get().isReference()) {
-			parseReferenceType(rt);
+			parseReferenceType(extractedMethod, rt);
 		}
 	}
 
-	private void parseReferenceType(Optional<ResolvedType> rt) {
+	private void parseReferenceType(MethodDeclaration extractedMethod, Optional<ResolvedType> rt) {
 		ResolvedReferenceType rrt = rt.get().asReferenceType();
 		ParseResult<Type> pr = new JavaParser().parseType(rrt.getQualifiedName());
 		if(pr.getResult().isPresent()) {
 			Type t = pr.getResult().get();
 			if(t.isReferenceType()) {
-				decl.addThrownException((ReferenceType)t);
+				extractedMethod.addThrownException((ReferenceType)t);
 			}
 		}
 	}
