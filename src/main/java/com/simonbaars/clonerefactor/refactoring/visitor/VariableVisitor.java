5013eac1fd49259c4b0283374b05de28b9261af1
Simon Baars <simon.mailadres@gmail.com>
2019-07-31 15:24:23 +0200

    Update VariableVisitor.java

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -3,18 +3,20 @@ package com.simonbaars.clonerefactor.refactoring.visitor;
 import java.util.Optional;
 
 import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
 import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
 
 public class VariableVisitor extends VoidVisitorAdapter<Void> implements ResolvesSymbols {
 
 	@Override
-	public void visit(MethodCallExpr md, Void arg) {
-		super.visit(md, arg);
-		Optional<ResolvedMethodDeclaration> mr = resolve(md::resolve);
+	public void visit(NameExpr ne, Void arg) {
+		super.visit(ne, arg);
+		Optional<ResolvedValueDeclaration> mr = resolve(ne::resolve);
 		if(mr.isPresent()) {
-			System.out.println(mr.get().getQualifiedSignature());
+			System.out.println(mr.get().);
 		}
 	}
 }
