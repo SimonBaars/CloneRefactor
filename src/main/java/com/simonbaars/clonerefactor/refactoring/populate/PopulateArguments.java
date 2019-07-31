1bd1d9e149323069c627fb13ab06dfad505012bf
Simon Baars <simon.mailadres@gmail.com>
2019-07-31 16:25:22 +0200

    Populate Arguments

--- src/main/java/com/simonbaars/clonerefactor/refactoring/populate/PopulateArguments.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/populate/PopulateArguments.java
@@ -1,33 +1,33 @@
 package com.simonbaars.clonerefactor.refactoring.populate;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map.Entry;
 
-import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.resolution.types.ResolvedType;
-import com.simonbaars.clonerefactor.model.Sequence;
+import com.simonbaars.clonerefactor.refactoring.visitor.DeclaresVariableVisitor;
 import com.simonbaars.clonerefactor.refactoring.visitor.VariableVisitor;
 
 public class PopulateArguments implements PopulatesExtractedMethod {
 	public PopulateArguments() {}
 
 	@Override
-	public void execute(MethodDeclaration extractedMethod, Sequence sequence) {
+	public void execute(MethodDeclaration extractedMethod) {
 		HashMap<NameExpr, ResolvedType> usedVariables = new HashMap<>();
 		extractedMethod.accept(new VariableVisitor(), usedVariables);
 		for(Entry<NameExpr, ResolvedType> var : usedVariables.entrySet()) {
-			if(!declaresVariable(sequence.getAny().getContents().getNodes(), var.getValue())) {
+			if(!declaresVariable(extractedMethod, var.getKey())) {
 				extractedMethod.addParameter(var.getValue().toString(), var.getKey().getNameAsString());
 			}
 		}
 	}
 
-	private boolean declaresVariable(List<Node> nodes, ResolvedType value) {
-		// TODO Auto-generated method stub
-		return false;
+	private boolean declaresVariable(MethodDeclaration extractedMethod, NameExpr varName) {
+		Boolean b = extractedMethod.accept(new DeclaresVariableVisitor(), varName);
+		if(b == null)
+			return false;
+		return b;
 	}
 
 }
