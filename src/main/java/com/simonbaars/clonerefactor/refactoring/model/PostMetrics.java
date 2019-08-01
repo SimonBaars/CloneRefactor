722dbcaded600ecf53bcf483970161d1ff22fc49
Simon Baars <simon.mailadres@gmail.com>
2019-08-01 17:31:01 +0200

    Working on post metrics

---
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/model/PostMetrics.java
@@ -0,0 +1,36 @@
+package com.simonbaars.clonerefactor.refactoring.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.Statement;
+import com.simonbaars.clonerefactor.metrics.collectors.CyclomaticComplexityCalculator;
+import com.simonbaars.clonerefactor.metrics.context.interfaces.RequiresNodeContext;
+import com.simonbaars.clonerefactor.model.Sequence;
+import com.simonbaars.clonerefactor.model.location.Location;
+
+public class PostMetrics implements RequiresNodeContext {
+	private final Map<MethodDeclaration, Integer> cc = new HashMap<>();
+	private final Map<MethodDeclaration, Integer> size = new HashMap<>();
+	
+	public Map<MethodDeclaration, Integer> getCc() {
+		return cc;
+	}
+	public Map<MethodDeclaration, Integer> getSize() {
+		return size;
+	}
+	
+	public void determine(MethodDeclaration newMethod, Optional<ClassOrInterfaceDeclaration> classOrInterface, List<Statement> methodcalls, Sequence s) {
+		for(Location location : s.getLocations()) {
+			Optional<MethodDeclaration> locationMethod = getMethod(location.getFirstNode());
+			if(locationMethod.isPresent()) {
+				cc.put(locationMethod.get(), new CyclomaticComplexityCalculator().calculate(locationMethod.get()));
+				size.put(locationMethod.get(), new CyclomaticComplexityCalculator().calculate(locationMethod.get()));
+			}
+		}
+	}
+}
