3f75ba2223069ff4f668e5f0d0a9b4a3f4a7a705
Simon Baars <simon.mailadres@gmail.com>
2019-08-01 13:58:31 +0200

    Removed the static part from the sequence observable
    rip codearena

--- src/main/java/com/simonbaars/clonerefactor/SequenceObservable.java
+++ src/main/java/com/simonbaars/clonerefactor/SequenceObservable.java
@@ -8,11 +8,6 @@ import com.simonbaars.clonerefactor.model.Sequence;
 
 public class SequenceObservable {
 	private final List<SequenceObserver> observers = new ArrayList<>();
-	private static final SequenceObservable singleton = new SequenceObservable();
-	
-	public static SequenceObservable get() {
-		return singleton;
-	}
 	
 	public void sendUpdate(ProblemType problem, Sequence sequence, int problemSize) {
 		observers.forEach(e -> e.update(problem, sequence, problemSize));
--- src/main/java/com/simonbaars/clonerefactor/metrics/ProblemType.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/ProblemType.java
@@ -15,4 +15,9 @@ public enum ProblemType {
 	public String getName() {
 		return name;
 	}
+	
+	@Override
+	public String toString() {
+		return getName();
+	}
 }
