bc191c5e7f3f302ae049872d8f976ffeec9d2bee
Simon Baars <simon.mailadres@gmail.com>
2019-08-01 08:42:39 +0200

    Tbh, I broke everything

--- src/main/java/com/simonbaars/clonerefactor/refactoring/populate/PopulatesExtractedMethod.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/populate/PopulatesExtractedMethod.java
@@ -1,7 +1,13 @@
 package com.simonbaars.clonerefactor.refactoring.populate;
 
+import java.util.List;
+
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 
 public interface PopulatesExtractedMethod {
-	public void execute(MethodDeclaration extractedMethod);
+	public void prePopulate(MethodDeclaration extractedMethod, List<Node> topLevel);
+	public void modifyMethodCall(MethodCallExpr expr);
+	public void postPopulate(MethodDeclaration extractedMethod);
 }
