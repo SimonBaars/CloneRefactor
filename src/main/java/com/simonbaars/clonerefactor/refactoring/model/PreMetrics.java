4e58820858335aa6bd4d256f8c3a788bce1bde78
Simon Baars <simon.mailadres@gmail.com>
2019-08-02 17:52:22 +0200

    Don't think this matters

--- src/main/java/com/simonbaars/clonerefactor/refactoring/model/PreMetrics.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/model/PreMetrics.java
@@ -11,20 +11,11 @@ import com.simonbaars.clonerefactor.model.Sequence;
 import com.simonbaars.clonerefactor.model.location.Location;
 
 public class PreMetrics implements RequiresNodeContext {
-	private final Map<MethodDeclaration, Integer> cc = new HashMap<>();
-	private final Map<MethodDeclaration, Integer> size = new HashMap<>();
 	private final int totalTokens;
 	private final int totalNodes;
 	private final int totalLines;
 	
 	public PreMetrics(Sequence s) {
-		for(Location location : s.getLocations()) {
-			Optional<MethodDeclaration> locationMethod = getMethod(location.getFirstNode());
-			if(locationMethod.isPresent()) {
-				cc.put(locationMethod.get(), new CyclomaticComplexityCalculator().calculate(locationMethod.get()));
-				size.put(locationMethod.get(), new CyclomaticComplexityCalculator().calculate(locationMethod.get()));
-			}
-		}
 		this.totalTokens = s.getTotalTokenVolume();
 		this.totalNodes = s.getTotalNodeVolume();
 		this.totalLines = s.getTotalEffectiveLineVolume();
