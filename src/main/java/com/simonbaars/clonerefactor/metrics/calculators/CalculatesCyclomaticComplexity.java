2319a3a390dc9c8b0b6694c1d0bb496a02fac245
Simon Baars <simon.mailadres@gmail.com>
2019-08-03 08:38:16 +0200

    Calculates complexity for lists

--- src/main/java/com/simonbaars/clonerefactor/metrics/calculators/CalculatesCyclomaticComplexity.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/calculators/CalculatesCyclomaticComplexity.java
@@ -1,5 +1,6 @@
 package com.simonbaars.clonerefactor.metrics.calculators;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,26 +17,58 @@ import com.github.javaparser.ast.stmt.WhileStmt;
 public interface CalculatesCyclomaticComplexity {
 	public default int calculateCyclomaticComplexity(Node node) {
 		List<IfStmt> ifStmts = node.findAll(IfStmt.class);
-        List<ForStmt> forStmts = node.findAll(ForStmt.class);
-        List<WhileStmt> whileStmts = node.findAll(WhileStmt.class);
-        List<DoStmt> doStmts = node.findAll(DoStmt.class);
-        List<SwitchEntry> catchStmts = node.findAll(SwitchEntry.class).stream().
-                filter(s -> !s.getLabels().isEmpty()) //Don't include "default" statements, only labeled case statements
-                .collect(Collectors.toList());
-        List<ConditionalExpr> ternaryExprs = node.findAll(ConditionalExpr.class);
-        List<BinaryExpr> andExprs = node.findAll(BinaryExpr.class).stream().
-                filter(f -> f.getOperator() == Operator.AND).collect(Collectors.toList());
-        List<BinaryExpr> orExprs = node.findAll(BinaryExpr.class).stream().
-                filter(f -> f.getOperator() == Operator.OR).collect(Collectors.toList());
+		List<ForStmt> forStmts = node.findAll(ForStmt.class);
+		List<WhileStmt> whileStmts = node.findAll(WhileStmt.class);
+		List<DoStmt> doStmts = node.findAll(DoStmt.class);
+		List<SwitchEntry> catchStmts = node.findAll(SwitchEntry.class).stream().
+				filter(s -> !s.getLabels().isEmpty()) //Don't include "default" statements, only labeled case statements
+				.collect(Collectors.toList());
+		List<ConditionalExpr> ternaryExprs = node.findAll(ConditionalExpr.class);
+		List<BinaryExpr> andExprs = node.findAll(BinaryExpr.class).stream().
+				filter(f -> f.getOperator() == Operator.AND).collect(Collectors.toList());
+		List<BinaryExpr> orExprs = node.findAll(BinaryExpr.class).stream().
+				filter(f -> f.getOperator() == Operator.OR).collect(Collectors.toList());
 
-       return ifStmts.size() +
-                forStmts.size() +
-                whileStmts.size() +
-                doStmts.size() +
-                catchStmts.size() +
-                ternaryExprs.size() +
-                andExprs.size() +
-                orExprs.size() +
-                1;
+		return ifStmts.size() +
+				forStmts.size() +
+				whileStmts.size() +
+				doStmts.size() +
+				catchStmts.size() +
+				ternaryExprs.size() +
+				andExprs.size() +
+				orExprs.size() +
+				1;
+	}
+
+	public default int calculateCyclomaticComplexity(Node...nodes) {
+		return calculateCyclomaticComplexity(Arrays.asList(nodes));
+	}
+
+	public default int calculateCyclomaticComplexity(List<Node> nodes) {
+		int total = 1;
+		for(Node node : nodes) {
+			List<IfStmt> ifStmts = node.findAll(IfStmt.class);
+			List<ForStmt> forStmts = node.findAll(ForStmt.class);
+			List<WhileStmt> whileStmts = node.findAll(WhileStmt.class);
+			List<DoStmt> doStmts = node.findAll(DoStmt.class);
+			List<SwitchEntry> catchStmts = node.findAll(SwitchEntry.class).stream().
+					filter(s -> !s.getLabels().isEmpty()) //Don't include "default" statements, only labeled case statements
+					.collect(Collectors.toList());
+			List<ConditionalExpr> ternaryExprs = node.findAll(ConditionalExpr.class);
+			List<BinaryExpr> andExprs = node.findAll(BinaryExpr.class).stream().
+					filter(f -> f.getOperator() == Operator.AND).collect(Collectors.toList());
+			List<BinaryExpr> orExprs = node.findAll(BinaryExpr.class).stream().
+					filter(f -> f.getOperator() == Operator.OR).collect(Collectors.toList());
+
+			total+= ifStmts.size() +
+					forStmts.size() +
+					whileStmts.size() +
+					doStmts.size() +
+					catchStmts.size() +
+					ternaryExprs.size() +
+					andExprs.size() +
+					orExprs.size();
+		}
+		return total;
 	}
 }
