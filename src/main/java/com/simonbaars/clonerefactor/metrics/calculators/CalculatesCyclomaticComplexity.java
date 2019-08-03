3cfd1cfe267bf7dc5b98ad2f9744604428520a09
Simon Baars <simon.mailadres@gmail.com>
2019-08-03 08:26:14 +0200

    Calculates Cyclomatic Complexity
    The calculators are pretty much redundant now

---
+++ src/main/java/com/simonbaars/clonerefactor/metrics/calculators/CalculatesCyclomaticComplexity.java
@@ -0,0 +1,41 @@
+package com.simonbaars.clonerefactor.metrics.calculators;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.BinaryExpr.Operator;
+import com.github.javaparser.ast.expr.ConditionalExpr;
+import com.github.javaparser.ast.stmt.DoStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.SwitchEntry;
+import com.github.javaparser.ast.stmt.WhileStmt;
+
+public interface CalculatesCyclomaticComplexity {
+	public default int calculateCyclomaticComplexity(Node node) {
+		List<IfStmt> ifStmts = node.findAll(IfStmt.class);
+        List<ForStmt> forStmts = node.findAll(ForStmt.class);
+        List<WhileStmt> whileStmts = node.findAll(WhileStmt.class);
+        List<DoStmt> doStmts = node.findAll(DoStmt.class);
+        List<SwitchEntry> catchStmts = node.findAll(SwitchEntry.class).stream().
+                filter(s -> !s.getLabels().isEmpty()) //Don't include "default" statements, only labeled case statements
+                .collect(Collectors.toList());
+        List<ConditionalExpr> ternaryExprs = node.findAll(ConditionalExpr.class);
+        List<BinaryExpr> andExprs = node.findAll(BinaryExpr.class).stream().
+                filter(f -> f.getOperator() == Operator.AND).collect(Collectors.toList());
+        List<BinaryExpr> orExprs = node.findAll(BinaryExpr.class).stream().
+                filter(f -> f.getOperator() == Operator.OR).collect(Collectors.toList());
+
+       return ifStmts.size() +
+                forStmts.size() +
+                whileStmts.size() +
+                doStmts.size() +
+                catchStmts.size() +
+                ternaryExprs.size() +
+                andExprs.size() +
+                orExprs.size() +
+                1;
+	}
+}
--- src/main/java/com/simonbaars/clonerefactor/metrics/calculators/CyclomaticComplexityCalculator.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/calculators/CyclomaticComplexityCalculator.java
@@ -1,44 +1,10 @@
 package com.simonbaars.clonerefactor.metrics.calculators;
 
-import static com.github.javaparser.ast.expr.BinaryExpr.Operator.AND;
-import static com.github.javaparser.ast.expr.BinaryExpr.Operator.OR;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.expr.ConditionalExpr;
-import com.github.javaparser.ast.stmt.DoStmt;
-import com.github.javaparser.ast.stmt.ForStmt;
-import com.github.javaparser.ast.stmt.IfStmt;
-import com.github.javaparser.ast.stmt.SwitchEntry;
-import com.github.javaparser.ast.stmt.WhileStmt;
 
-public class CyclomaticComplexityCalculator implements Calculator<MethodDeclaration> {
+public class CyclomaticComplexityCalculator implements Calculator<MethodDeclaration>, CalculatesCyclomaticComplexity {
     @Override
     public int calculate(MethodDeclaration method) {
-        List<IfStmt> ifStmts = method.findAll(IfStmt.class);
-        List<ForStmt> forStmts = method.findAll(ForStmt.class);
-        List<WhileStmt> whileStmts = method.findAll(WhileStmt.class);
-        List<DoStmt> doStmts = method.findAll(DoStmt.class);
-        List<SwitchEntry> catchStmts = method.findAll(SwitchEntry.class).stream().
-                filter(s -> !s.getLabels().isEmpty()) //Don't include "default" statements, only labeled case statements
-                .collect(Collectors.toList());
-        List<ConditionalExpr> ternaryExprs = method.findAll(ConditionalExpr.class);
-        List<BinaryExpr> andExprs = method.findAll(BinaryExpr.class).stream().
-                filter(f -> f.getOperator() == AND).collect(Collectors.toList());
-        List<BinaryExpr> orExprs = method.findAll(BinaryExpr.class).stream().
-                filter(f -> f.getOperator() == OR).collect(Collectors.toList());
-
-       return ifStmts.size() +
-                forStmts.size() +
-                whileStmts.size() +
-                doStmts.size() +
-                catchStmts.size() +
-                ternaryExprs.size() +
-                andExprs.size() +
-                orExprs.size() +
-                1;
+        return calculateCyclomaticComplexity(method);
     }
 }
