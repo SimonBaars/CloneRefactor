e82bf99d06234db12bea3f30d4b1dd0df6817595
Simon Baars <simon.mailadres@gmail.com>
2019-08-05 20:57:30 +0200

    Risk for problem types

--- src/main/java/com/simonbaars/clonerefactor/metrics/ProblemType.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/ProblemType.java
@@ -1,16 +1,22 @@
 package com.simonbaars.clonerefactor.metrics;
 
 public enum ProblemType {
-	DUPLICATION("Duplication"),
-	UNITINTERFACESIZE("Unit Interface Size"),
-	UNITCOMPLEXITY("Unit Complexity"),
-	LINEVOLUME("Line Volume"),
-	TOKENVOLUME("Token Volume");
+	DUPLICATION("Duplication", 2, 4, 6),
+	UNITINTERFACESIZE("Unit Interface Size", 2, 4, 6),
+	UNITCOMPLEXITY("Unit Complexity", 10, 20, 50),
+	LINEVOLUME("Line Volume", 15, 30, 60),
+	TOKENVOLUME("Token Volume", 15*10, 30*10, 60*10);
 	
 	private final String name;
+	private final int low;
+	private final int mid;
+	private final int high;
 	
-	private ProblemType(String name) {
+	private ProblemType(String name, int low, int mid, int high) {
 		this.name= name;
+		this.low=low;
+		this.mid=mid;
+		this.high=high;
 	}
 
 	public String getName() {
