e0e2be930e48c6d83a336c61b0128ce3cd3a6540
Simon Baars <simon.mailadres@gmail.com>
2019-08-05 21:00:53 +0200

    Risk string

--- src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/Risk.java
+++ src/main/java/com/simonbaars/clonerefactor/metrics/context/enums/Risk.java
@@ -1,5 +1,19 @@
 package com.simonbaars.clonerefactor.metrics.context.enums;
 
 public enum Risk {
-	LOW, MODERATE, HIGH, VERYHIGH
+	LOW("Low"), 
+	MODERATE("Moderate"), 
+	HIGH("High"), 
+	VERYHIGH("Very high");
+	
+	private final String name;
+	
+	private Risk(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 }
