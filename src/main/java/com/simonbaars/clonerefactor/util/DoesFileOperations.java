a6651ea801bf1c836babe2aac37a99dc0c691560
Simon Baars <simon.mailadres@gmail.com>
2019-08-05 08:15:33 +0200

    Delete file operation

--- src/main/java/com/simonbaars/clonerefactor/util/DoesFileOperations.java
+++ src/main/java/com/simonbaars/clonerefactor/util/DoesFileOperations.java
@@ -43,5 +43,19 @@ public interface DoesFileOperations {
 	        throw new RuntimeException(e.getMessage(), e);
 	    }
 	}
+	
+	public default boolean delete(File dir) {
+	    if (dir.isDirectory()) {
+	        String[] children = dir.list();
+	        for (int i=0; i<children.length; i++) {
+	            boolean success = delete(new File(dir, children[i]));
+	            if (!success) {
+	                return false;
+	            }
+	        }
+	    }
+	    return dir.delete();
+	}
+
 
 }
