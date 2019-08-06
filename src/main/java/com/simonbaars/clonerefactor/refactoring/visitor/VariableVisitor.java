4678dcab0efafdb3caded28601bcb4c68bf52eb7
Simon Baars <simon.mailadres@gmail.com>
2019-08-06 23:06:39 +0200

    Seek parents

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -4,14 +4,22 @@ import java.util.Map;
 import java.util.Optional;
 
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
+import com.github.javaparser.ast.nodeTypes.NodeWithType;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
+import com.simonbaars.clonerefactor.refactoring.visitor.ResolvedVariable.VariableType;
 
 public class VariableVisitor extends VoidVisitorAdapter<Map<NameExpr, ResolvedType>> implements ResolvesSymbols {
 
@@ -28,25 +36,41 @@ public class VariableVisitor extends VoidVisitorAdapter<Map<NameExpr, ResolvedTy
 		}
 	}
 	
-	public ResolvedVariable findDeclaration(NameExpr variable) {
-		if(variable.)
-		Optional<VariableDeclarator> seekParents = seekParents(variable);
-		return Optional.empty();
+	public Optional<ResolvedVariable> findDeclaration(NameExpr variable) {
+		Optional<ResolvedVariable> seekParents = seekParents(variable);
+		return seekParents;
 	}
 
-	private Optional<? extends Node> seekParents(NameExpr variable) {
+	private Optional<ResolvedVariable> seekParents(NameExpr variable) {
 		if(variable.getParentNode().isPresent()) {
 			Node parent = variable.getParentNode().get();
 			if(parent instanceof MethodDeclaration) {
 				Optional<Parameter> declaration = ((MethodDeclaration)parent).getParameters().stream().filter(parameter -> parameter.getName().equals(variable.getName())).findAny();
 				if(declaration.isPresent())
-					return declaration;
+					return createResolvedVariable(declaration.get()); 
 			}
-			if(parent instanceof BlockStmt)
-			for(Node child : parent.getChildNodes()) {
-				
+			if(parent instanceof BlockStmt) {
+				for(Node child : parent.getChildNodes()) {
+					if(child instanceof ExpressionStmt && ((ExpressionStmt)child).getExpression() instanceof VariableDeclarationExpr) {
+						VariableDeclarationExpr dec = (VariableDeclarationExpr)((ExpressionStmt)child).getExpression();
+						for(VariableDeclarator decl : dec.getVariables()) {
+							if(decl.getName().equals(variable.getName()))
+								return createResolvedVariable(decl); 
+						}
+					}
+				}
+			}
+			if(parent instanceof ClassOrInterfaceDeclaration && !((ClassOrInterfaceDeclaration)parent).isInterface()) {
+				ClassOrInterfaceDeclaration classDecl = (ClassOrInterfaceDeclaration)parent;
+				Optional<VariableDeclarator> declaration = classDecl.getMembers().stream().filter(e -> e instanceof FieldDeclaration).flatMap(e -> ((FieldDeclaration)e).getVariables().stream()).filter(e -> e.getName().equals(variable.getName())).findAny();
+				if(declaration.isPresent()) 
+					return createResolvedVariable(declaration.get());
 			}
 		}
 		return Optional.empty();
+	}
+
+	private<T extends Node & NodeWithType & NodeWithSimpleName> Optional<ResolvedVariable> createResolvedVariable(T decl) {
+		return Optional.of(new ResolvedVariable(decl.getType(), decl.getName(), decl, VariableType.METHODPARAMETER));
 	}
 }
