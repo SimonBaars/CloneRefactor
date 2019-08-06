32b20c0b49e20cb38ebe81731d420afc0864c70c
Simon Baars <simon.mailadres@gmail.com>
2019-08-06 22:27:14 +0200

    Creating a variable resolver
    it's still a mess I know

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -3,7 +3,11 @@ package com.simonbaars.clonerefactor.refactoring.visitor;
 import java.util.Map;
 import java.util.Optional;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
 import com.github.javaparser.resolution.types.ResolvedType;
@@ -22,5 +26,27 @@ public class VariableVisitor extends VoidVisitorAdapter<Map<NameExpr, ResolvedTy
 				//e.printStackTrace(); TODO TODO
 			}
 		}
+	}
+	
+	public ResolvedVariable findDeclaration(NameExpr variable) {
+		if(variable.)
+		Optional<VariableDeclarator> seekParents = seekParents(variable);
+		return Optional.empty();
+	}
+
+	private Optional<? extends Node> seekParents(NameExpr variable) {
+		if(variable.getParentNode().isPresent()) {
+			Node parent = variable.getParentNode().get();
+			if(parent instanceof MethodDeclaration) {
+				Optional<Parameter> declaration = ((MethodDeclaration)parent).getParameters().stream().filter(parameter -> parameter.getName().equals(variable.getName())).findAny();
+				if(declaration.isPresent())
+					return declaration;
+			}
+			if(parent instanceof BlockStmt)
+			for(Node child : parent.getChildNodes()) {
+				
+			}
+		}
+		return Optional.empty();
 	}
 }
