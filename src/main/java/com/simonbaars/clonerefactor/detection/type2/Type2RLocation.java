e4a614a231277844d8957dd7af342f14cacae754
Simon Baars <simon.mailadres@gmail.com>
2019-08-06 09:17:24 +0200

    Type 2 R location

---
+++ src/main/java/com/simonbaars/clonerefactor/detection/type2/Type2RLocation.java
@@ -0,0 +1,38 @@
+package com.simonbaars.clonerefactor.detection.type2;
+
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.javaparser.Range;
+import com.github.javaparser.ast.Node;
+import com.simonbaars.clonerefactor.model.location.Location;
+
+public class Type2RLocation extends Location {
+	
+	private final Set<Integer> diffIndices = new HashSet<>();
+
+	public Type2RLocation(Location clonedLocation, Range r) {
+		super(clonedLocation, r);
+	}
+
+	public Type2RLocation(Location clonedLocation) {
+		super(clonedLocation);
+	}
+
+	public Type2RLocation(Path path, Location prevLocation, Node n) {
+		super(path, prevLocation, n);
+	}
+
+	public Type2RLocation(Path file, Node... nodes) {
+		super(file, nodes);
+	}
+
+	public Type2RLocation(Path file, Range range) {
+		super(file, range);
+	}
+
+	public Set<Integer> getDiffIndices() {
+		return diffIndices;
+	}
+}
