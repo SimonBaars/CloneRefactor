5fe7b3de9a715537139cc570273b0f1449f66dc9
Simon Baars <simon.mailadres@gmail.com>
2019-08-06 09:39:04 +0200

    Update RiskProfile.java

--- src/main/java/com/simonbaars/clonerefactor/refactoring/model/RiskProfile.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/model/RiskProfile.java
@@ -3,6 +3,7 @@ package com.simonbaars.clonerefactor.refactoring.model;
 import java.util.EnumMap;
 import java.util.Map;
 
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.utils.Pair;
 import com.simonbaars.clonerefactor.datatype.map.CountMap;
 import com.simonbaars.clonerefactor.metrics.ProblemType;
@@ -12,10 +13,14 @@ public class RiskProfile {
 	private final Map<Risk, Integer> riskCC = new EnumMap<Risk, Integer>(Risk.class);
 	private final CountMap<Pair<Risk, Risk>> bucketChange = new CountMap<Pair<Risk, Risk>>();
 	private final ProblemType type;
+	private final Risk newMethodRisk;
+	private final int newMethodProblemSize;
 	
-	public RiskProfile(ProblemType type) {
+	public RiskProfile(ProblemType type, int newMethodProblemSize) {
 		super();
 		this.type = type;
+		this.newMethodProblemSize = newMethodProblemSize;
+		this.newMethodRisk = type.getRisk(newMethodProblemSize);
 	}
 	
 	public Map<Risk, Integer> getRiskCC() {
@@ -28,5 +33,26 @@ public class RiskProfile {
 	
 	public ProblemType getType() {
 		return type;
+	}
+	
+	public Risk getNewMethodRisk() {
+		return newMethodRisk;
+	}
+
+	public int getNewMethodProblemSize() {
+		return newMethodProblemSize;
+	}
+
+	public RiskProfile calculateRisk(Map<MethodDeclaration, Integer> methodCC2, Map<MethodDeclaration, Integer> methodCC3) {
+		methodCC2.entrySet().forEach(e -> {
+			if(methodCC3.containsKey(e.getKey())) {
+				Integer preRiskAmount = methodCC3.get(e.getKey());
+				Risk preRisk = type.getRisk(preRiskAmount);
+				Risk afterRisk = type.getRisk(e.getValue());
+				getBucketChange().increment(new Pair<Risk, Risk>(preRisk, afterRisk));
+				getRiskCC().put(preRisk, e.getValue() - preRiskAmount);
+			}
+		});
+		return this;
 	}
 }
