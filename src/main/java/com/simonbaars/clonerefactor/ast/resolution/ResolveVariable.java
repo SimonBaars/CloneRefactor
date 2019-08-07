215f114dcbf1965790c8dce8be881b0f781838ad
Simon Baars <simon.mailadres@gmail.com>
2019-08-07 09:56:37 +0200

    Resolve variable progress

--- src/main/java/com/simonbaars/clonerefactor/ast/resolution/ResolveVariable.java
+++ src/main/java/com/simonbaars/clonerefactor/ast/resolution/ResolveVariable.java
@@ -1,5 +1,6 @@
 package com.simonbaars.clonerefactor.ast.resolution;
 
+import java.util.Map;
 import java.util.Optional;
 
 import com.github.javaparser.ast.Node;
@@ -20,18 +21,27 @@ import com.simonbaars.clonerefactor.refactoring.visitor.ResolvedVariable;
 import com.simonbaars.clonerefactor.refactoring.visitor.ResolvedVariable.VariableType;
 
 public class ResolveVariable {
-	public Optional<ResolvedVariable> findDeclaration(SimpleName variable) {
+	
+	private final Map<String, ClassOrInterfaceDeclaration> classes;
+	private final SimpleName variable;
+
+	public ResolveVariable (Map<String, ClassOrInterfaceDeclaration> classes, SimpleName variable) {
+		this.classes = classes;
+		this.variable = variable;
+	}
+	
+	public Optional<ResolvedVariable> findDeclaration() {
 		if(variable.getParentNode().isPresent() && variable.getParentNode().get() instanceof FieldAccessExpr) {
 			FieldAccessExpr fieldAccess = (FieldAccessExpr)variable.getParentNode().get();
-			if(fieldAccess.getScope() instanceof ThisExpr && getDeclaratorOfVariableForClass(variable, getClass())) {
+			if(fieldAccess.getScope() instanceof ThisExpr && getDeclaratorOfVariableForClass(getClass())) {
 				
 			}
 		}
-		Optional<ResolvedVariable> seekParents = seekParents(variable, variable);
+		Optional<ResolvedVariable> seekParents = seekParents(variable);
 		return seekParents;
 	}
 
-	private Optional<ResolvedVariable> seekParents(SimpleName variable, Node node) {
+	private Optional<ResolvedVariable> seekParents(Node node) {
 		if(node.getParentNode().isPresent()) {
 			Node parent = node.getParentNode().get();
 			if(parent instanceof MethodDeclaration) {
@@ -52,23 +62,21 @@ public class ResolveVariable {
 			}
 			else if(parent instanceof ClassOrInterfaceDeclaration && !((ClassOrInterfaceDeclaration)parent).isInterface()) {
 				ClassOrInterfaceDeclaration classDecl = (ClassOrInterfaceDeclaration)parent;
-				Optional<VariableDeclarator> declaration = getDeclaratorOfVariableForClass(variable, classDecl);
+				Optional<VariableDeclarator> declaration = getDeclaratorOfVariableForClass(classDecl);
 				if(declaration.isPresent()) 
 					return createResolvedVariable(declaration.get());
-				classDecl.getExtendedTypes().stream().map(e -> findVariableInSuperclass(null, variable));
 			}
-			seekParents(variable, parent);
+			seekParents(parent);
 		}
 		return Optional.empty();
 	}
 	
-	private Optional<ResolvedVariable> findVariableInSuperclass(SimpleName variable){
-		
+	private Optional<ResolvedVariable> findVariableInSuperclass(ClassOrInterfaceDeclaration classDecl){
+		classDecl.getExtendedTypes().stream().map(e -> findVariableInSuperclass(null, variable));
 		return Optional.empty();
 	}
 
-	private Optional<VariableDeclarator> getDeclaratorOfVariableForClass(SimpleName variable,
-			ClassOrInterfaceDeclaration classDecl) {
+	private Optional<VariableDeclarator> getDeclaratorOfVariableForClass(ClassOrInterfaceDeclaration classDecl) {
 		return classDecl.getMembers().stream().filter(e -> e instanceof FieldDeclaration).flatMap(e -> ((FieldDeclaration)e).getVariables().stream()).filter(e -> e.getName().equals(variable)).findAny();
 	}
 
