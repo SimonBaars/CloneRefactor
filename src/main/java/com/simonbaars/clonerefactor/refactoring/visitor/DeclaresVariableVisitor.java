fdecd150ea7f6003f4abc9288301a54368690442
Simon Baars <simon.mailadres@gmail.com>
2019-08-07 14:59:39 +0200

    Visitors

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/DeclaresVariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/DeclaresVariableVisitor.java
@@ -1,15 +1,15 @@
 package com.simonbaars.clonerefactor.refactoring.visitor;
 
 import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
-public class DeclaresVariableVisitor extends GenericVisitorAdapter<Boolean, NameExpr> {
+public class DeclaresVariableVisitor extends GenericVisitorAdapter<Boolean, SimpleName> {
     @Override
-    public Boolean visit(final VariableDeclarator n, final NameExpr var) {
+    public Boolean visit(final VariableDeclarator n, final SimpleName var) {
     	super.visit(n, var);
   
-    	if(n.getName().equals(var.getName()))
+    	if(n.getName().equals(var))
         	return true;
         return null;
     }
--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -3,39 +3,44 @@ package com.simonbaars.clonerefactor.refactoring.visitor;
 import java.util.Map;
 import java.util.Optional;
 
-import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.expr.FieldAccessExpr;
-import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.SimpleName;
-import com.github.javaparser.ast.expr.ThisExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
-import com.github.javaparser.ast.nodeTypes.NodeWithType;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
-import com.simonbaars.clonerefactor.ast.resolution.ResolvedVariable.VariableType;
+import com.simonbaars.clonerefactor.ast.resolution.ResolveVariable;
+import com.simonbaars.clonerefactor.ast.resolution.ResolvedVariable;
 
-public class VariableVisitor extends VoidVisitorAdapter<Map<NameExpr, ResolvedType>> implements ResolvesSymbols {
+public class VariableVisitor extends VoidVisitorAdapter<Map<SimpleName, ResolvedType>> implements ResolvesSymbols {
+
+	private Map<String, ClassOrInterfaceDeclaration> classes;
+
+	public VariableVisitor(Map<String, ClassOrInterfaceDeclaration> classes) {
+		this.classes = classes;
+	}
 
 	@Override
-	public void visit(NameExpr ne, Map<NameExpr, ResolvedType> arg) {
+	public void visit(SimpleName ne, Map<SimpleName, ResolvedType> arg) {
 		super.visit(ne, arg);
-		Optional<ResolvedValueDeclaration> mr = resolve(ne::resolve);
-		if(mr.isPresent()) {
-			try { // GOES WRONG WAY TOO OFTEN! TODO TODO TODO	
-				arg.put(ne, mr.get().getType());
-			} catch (Exception e) {
-				//e.printStackTrace(); TODO TODO
+		System.out.println(ne);
+		Optional<ResolvedVariable> resolvedVar = new ResolveVariable(classes, ne).findDeclaration();
+		if(resolvedVar.isPresent()) {
+			Optional<ResolvedType> type = resolvedVar.get().resolveType();
+			System.out.println(resolvedVar.get().toString()+ ", "+type.isPresent());
+			if(type.isPresent()) {
+				arg.put(ne, type.get());
 			}
+			
+		} else {
+			System.out.println(ne+" not found");
 		}
+		//Optional<ResolvedValueDeclaration> mr = resolve(ne::resolve);
+		//if(mr.isPresent()) {
+		//	try { // GOES WRONG WAY TOO OFTEN! TODO TODO TODO	
+		//		arg.put(ne, mr.get().getType());
+		//	} catch (Exception e) {
+				//e.printStackTrace(); TODO TODO
+		//	}
+		//}
 	}
 }
