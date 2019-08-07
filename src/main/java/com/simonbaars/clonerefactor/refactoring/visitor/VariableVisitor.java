0d55297f20e70ebcc553d9d458ec57fdd8c65d55
Simon Baars <simon.mailadres@gmail.com>
2019-08-07 15:50:00 +0200

    Refactor

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -1,7 +1,6 @@
 package com.simonbaars.clonerefactor.refactoring.visitor;
 
 import java.util.Map;
-import java.util.Optional;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
@@ -21,21 +20,6 @@ public class VariableVisitor extends VoidVisitorAdapter<Map<SimpleName, Resolved
 	@Override
 	public void visit(SimpleName ne, Map<SimpleName, ResolvedVariable> arg) {
 		super.visit(ne, arg);
-		System.out.println(ne);
-		Optional<ResolvedVariable> resolvedVar = new ResolveVariable(classes, ne).findDeclaration();
-		if(resolvedVar.isPresent()) {
-			arg.put(ne, resolvedVar.get());
-			
-		} else {
-			System.out.println(ne+" not found");
-		}
-		//Optional<ResolvedValueDeclaration> mr = resolve(ne::resolve);
-		//if(mr.isPresent()) {
-		//	try { // GOES WRONG WAY TOO OFTEN! TODO TODO TODO	
-		//		arg.put(ne, mr.get().getType());
-		//	} catch (Exception e) {
-				//e.printStackTrace(); TODO TODO
-		//	}
-		//}
+		new ResolveVariable(classes, ne).findDeclaration().ifPresent(var -> arg.put(ne, var));
 	}
 }
