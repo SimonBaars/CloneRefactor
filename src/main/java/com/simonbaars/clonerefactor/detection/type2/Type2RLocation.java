b099d137bfe15549ab3381b32019f47ecc95c35e
Simon Baars <simon.mailadres@gmail.com>
2019-08-09 13:18:58 +0200

    add dummy parameters

--- src/main/java/com/simonbaars/clonerefactor/detection/type2/Type2RLocation.java
+++ src/main/java/com/simonbaars/clonerefactor/detection/type2/Type2RLocation.java
@@ -1,12 +1,19 @@
 package com.simonbaars.clonerefactor.detection.type2;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.LiteralExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.type.PrimitiveType;
 import com.simonbaars.clonerefactor.model.location.Location;
 
 public class Type2RLocation extends Location {
@@ -34,5 +41,26 @@ public class Type2RLocation extends Location {
 
 	public Set<Expression> getDiffExpressions() {
 		return diffExpressions;
+	}
+	
+	public void addDummyParameters(MethodDeclaration extractedMethod) {
+		getDummyParameters().stream().filter(p -> extractedMethod.getParameters().stream().noneMatch(a -> a.getNameAsString().equals(p))).forEach(p -> extractedMethod.addParameter(PrimitiveType.intType(), p));
+	}
+	
+	public void addDummyParameters(MethodCallExpr expr) {
+		getDummyParameters().stream().filter(p -> expr.getArguments().stream().noneMatch(a -> a.toString().equals(p))).forEach(p -> expr.addArgument(new NameExpr(p)));
+	}
+	
+	private List<String> getDummyParameters() {
+		int x = 1;
+		List<String> params = new ArrayList<String>();
+		for(Expression diff : diffExpressions) {
+			if(diff instanceof MethodCallExpr || diff instanceof LiteralExpr) {
+				params.add("dummy"+(x++));
+			} else {
+				params.add(((NameExpr)diff).getNameAsString());
+			}
+		}
+		return params;
 	}
 }
