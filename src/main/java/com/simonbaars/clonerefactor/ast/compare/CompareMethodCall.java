1dfda09f3d90ebc622adb286fdd42e35fa82cb3e
Simon <simon.mailadres@gmail.com>
2019-08-12 16:37:39 +0200

    Simplified method call compare

--- src/main/java/com/simonbaars/clonerefactor/ast/compare/CompareMethodCall.java
+++ src/main/java/com/simonbaars/clonerefactor/ast/compare/CompareMethodCall.java
@@ -2,7 +2,6 @@ package com.simonbaars.clonerefactor.ast.compare;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.github.javaparser.JavaToken;
@@ -11,26 +10,15 @@ import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.simonbaars.clonerefactor.ast.interfaces.HasCompareList;
 import com.simonbaars.clonerefactor.ast.interfaces.ResolvesSymbols;
-import com.simonbaars.clonerefactor.settings.CloneType;
 
 public class CompareMethodCall extends Compare implements ResolvesSymbols {
 	private final List<JavaToken> methodCall;
 	private final MethodCallExpr methodCallExpr;
-	private final Optional<MethodDeclarationProxy> type;
-	private final List<Object> estimatedTypes = new ArrayList<>();
 	
 	public CompareMethodCall(MethodCallExpr t) {
 		super(t.getRange().get());
 		this.methodCall = getPartsOfCall(t);
 		this.methodCallExpr = t;
-		type = resolve(() -> new MethodDeclarationProxy(t.resolve()));
-		if(!type.isPresent())
-			estimateTypes(t);
-	}
-
-	// I'm not so sure about this whole estimateTypes thing. The problem is that JavaParser cannot resolve everything. In essence, we cannot guarantee equality, thus this can result in invalid refactorings. Because of that, we *should* remove this estimateTypes thing, and just mark the equality `false` for all unresolved method calls.
-	private void estimateTypes(MethodCallExpr t) {
-		estimatedTypes.addAll(t.getArguments().stream().map(e -> resolve(e::calculateResolvedType)).collect(Collectors.toList()));
 	}
 	
 	@Override
@@ -38,11 +26,7 @@ public class CompareMethodCall extends Compare implements ResolvesSymbols {
 		if(!super.equals(c))
 			return false;
 		CompareMethodCall other = (CompareMethodCall)c;
-		if(getCloneType() == CloneType.TYPE1R && !methodCall.equals(other.methodCall))
-			return false;
-		if(type.isPresent() && other.type.isPresent())
-			return getCloneType().isNotType1() ? type.get().equalsType2(other.type.get()) : type.get().equalsType1(other.type.get());
-		return estimatedTypes.equals(other.estimatedTypes);
+		return methodCall.equals(other.methodCall);
 	}
 
 	private List<JavaToken> getPartsOfCall(MethodCallExpr methodCall2) {
@@ -57,17 +41,12 @@ public class CompareMethodCall extends Compare implements ResolvesSymbols {
 
 	@Override
 	public int hashCode() {
-		if(type.isPresent()) {
-			if(getCloneType().isNotType1())
-				return type.get().hashcodeType2();
-			return type.get().hashcodeType1();
-		}
-		return estimatedTypes.hashCode();
+		return methodCall.hashCode();
 	}
 
 	@Override
 	public String toString() {
-		return "CompareMethodCall [type=" + type + "]";
+		return "CompareMethodCall [type=" + methodCallExpr + "]";
 	}
 	
 	@Override
