1aeeef9c8c3615d24b6608e1073389fbdee5a031
Simon Baars <simon.mailadres@gmail.com>
2019-08-13 09:02:19 +0200

    Resolving my own FQIs doesn't sound all that bad currently

---
+++ src/main/java/com/simonbaars/clonerefactor/detection/interfaces/ResolvesFQIs.java
@@ -0,0 +1,51 @@
+package com.simonbaars.clonerefactor.detection.interfaces;
+
+import java.util.Optional;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.simonbaars.clonerefactor.ast.ASTHolder;
+import com.simonbaars.clonerefactor.metrics.context.interfaces.RequiresNodeContext;
+
+public interface ResolvesFQIs extends RequiresNodeContext {
+	public default String getFullyQualifiedName(ClassOrInterfaceDeclaration childClass, ClassOrInterfaceType t) {
+		String name = "";
+		if(t.getScope().isPresent())
+			name+=getFullyQualifiedName(childClass, t.getScope().get())+".";
+		else if(childClass!=null) {
+			Optional<CompilationUnit> compilationUnit = getCompilationUnit(childClass);
+			if(!compilationUnit.isPresent())
+				return t.asString();
+			Optional<String> nameOpt = compilationUnit.get().getImports().stream().map(e -> e.getNameAsString()).filter(e -> e.endsWith("."+t.getName())).findAny();
+			if(nameOpt.isPresent()) 
+				return nameOpt.get();
+			else {
+				String fullyQualifiedName = getFullyQualifiedName(childClass);
+				name+=fullyQualifiedName.substring(0, fullyQualifiedName.lastIndexOf('.')+1);
+				if(!ASTHolder.getClasses().containsKey(name+t.getNameAsString()) && compilationUnit.get().getImports().stream().anyMatch(e -> e.isAsterisk())) {
+					Optional<String> asteriks = compilationUnit.get().getImports().stream().filter(e -> e.isAsterisk()).map(e -> e.getNameAsString()+"."+t.getNameAsString()).filter(e -> ASTHolder.getClasses().containsKey(e)).findAny();
+					if(asteriks.isPresent())
+						return asteriks.get();
+				}
+			}
+		}
+		return name+t.getNameAsString();
+	}
+	
+	public default String getFullyQualifiedName(ClassOrInterfaceDeclaration c2) {
+		String name = "";
+		if(c2 == null)
+			return null;
+		Optional<ClassOrInterfaceDeclaration> parentClass = c2.getParentNode().isPresent() ? getClass(c2.getParentNode().get()) : Optional.empty();
+		if(parentClass.isPresent()) {
+			name+=getFullyQualifiedName(parentClass.get())+".";
+		} else {
+			Optional<CompilationUnit> u = getCompilationUnit(c2);
+			if(u.isPresent() && u.get().getPackageDeclaration().isPresent()) {
+				name+=u.get().getPackageDeclaration().get().getNameAsString()+".";
+			}
+		}
+		return name+c2.getNameAsString();
+	}
+}
