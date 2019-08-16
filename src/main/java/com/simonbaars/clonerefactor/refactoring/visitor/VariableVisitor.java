34ba17fb1842c0808d1041b94fcf1dfecc48cf57
Simon <simon.mailadres@gmail.com>
2019-08-16 16:54:24 +0200

    Cleanup unneeded stuffz

--- src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
+++ src/main/java/com/simonbaars/clonerefactor/refactoring/visitor/VariableVisitor.java
@@ -5,11 +5,10 @@ import java.util.Map;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.simonbaars.clonerefactor.graph.interfaces.ResolvesSymbols;
 import com.simonbaars.clonerefactor.graph.resolution.ResolveVariable;
 import com.simonbaars.clonerefactor.graph.resolution.ResolvedVariable;
 
-public class VariableVisitor extends VoidVisitorAdapter<Map<SimpleName, ResolvedVariable>> implements ResolvesSymbols {
+public class VariableVisitor extends VoidVisitorAdapter<Map<SimpleName, ResolvedVariable>> {
 
 	private Map<String, ClassOrInterfaceDeclaration> classes;
 
