9dad1228ec0df2e84447772b95867fa24e6a7ac0
Simon <simon.mailadres@gmail.com>
2019-10-30 07:25:14 +0100

    parse nodes

--- src/main/java/com/simonbaars/clonerefactor/scripts/intimals/IntimalsReader.java
+++ src/main/java/com/simonbaars/clonerefactor/scripts/intimals/IntimalsReader.java
@@ -2,6 +2,8 @@ package com.simonbaars.clonerefactor.scripts.intimals;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -9,9 +11,12 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import com.simonbaars.clonerefactor.scripts.intimals.model.Pattern;
 
 public class IntimalsReader {
 	private static int clusterNum = 1;
@@ -27,16 +32,26 @@ public class IntimalsReader {
 		parseMatches(doc.getElementsByTagName("matches"));
 	}
 
-	private static void parseMatches(NodeList matches) {
+	private static List<Pattern> parseMatches(NodeList matches) {
 		for(int i = 0; i<matches.getLength(); i++) {
 			Node node = matches.item(i);
 			if(node.getNodeType() == Node.ELEMENT_NODE) {
 				Element element = (Element)node;
-				parseNodes(element.getChildNodes());
+				parseNodes(element.getElementsByTagName("node"));
 			}
 		}
+		return null;
 	}
 
-	private static void parseNodes(NodeList nodes) {
+	private static List<com.simonbaars.clonerefactor.scripts.intimals.model.Node> parseNodes(NodeList nodes) {
+		List<com.simonbaars.clonerefactor.scripts.intimals.model.Node> parsedNodes = new ArrayList<>();
+		for(int i = 0; i<nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if(node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element)node;
+				parsedNodes.add(new com.simonbaars.clonerefactor.scripts.intimals.model.Node(Integer.parseInt(element.getAttribute("ID")), element.getAttribute("Name").equals("?_root")));
+			}
+		}
+		return parsedNodes;
 	}
 }
