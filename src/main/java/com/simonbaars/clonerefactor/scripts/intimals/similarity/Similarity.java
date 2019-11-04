f516ac816d6720579a452b26f2b8cc09188a861a
Simon <simon.mailadres@gmail.com>
2019-11-04 10:30:15 +0100

    Determine actual similarity

--- src/main/java/com/simonbaars/clonerefactor/scripts/intimals/similarity/Similarity.java
+++ src/main/java/com/simonbaars/clonerefactor/scripts/intimals/similarity/Similarity.java
@@ -1,11 +1,30 @@
 package com.simonbaars.clonerefactor.scripts.intimals.similarity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.simonbaars.clonerefactor.detection.model.location.Location;
 import com.simonbaars.clonerefactor.scripts.intimals.model.PatternLocation;
 
 public abstract class Similarity {
 
 	public Similarity() {}
+	
+	public static Similarity determineSimilarity(PatternLocation pattern, List<Location> clones) {
+		List<Similarity> similarities = new ArrayList<>();
+		for(Location clone : clones) {
+			similarities.add(determineSimilarity(pattern, clone));
+		}
+		return similarities.stream().reduce((e1, e2) -> e1.isMoreImportant(e2) ? e1 : e2).get();
+	}
+	
+	public static Similarity determineSimilarity(List<PatternLocation> patterns, Location clone) {
+		List<Similarity> similarities = new ArrayList<>();
+		for(PatternLocation pattern : patterns) {
+			similarities.add(determineSimilarity(pattern, clone));
+		}
+		return similarities.stream().reduce((e1, e2) -> e1.isMoreImportant(e2) ? e1 : e2).get();
+	}
 
 	private static Similarity determineSimilarity(PatternLocation pattern, Location clone) {
 		if(!pattern.getFile().equals(clone.getFile())) {
