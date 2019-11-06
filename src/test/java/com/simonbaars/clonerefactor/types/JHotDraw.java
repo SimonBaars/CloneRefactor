7f5d1ac32abd23725218b3d50877800cf11c22ec
Simon <simon.mailadres@gmail.com>
2019-11-06 10:50:12 +0100

    Collect the data and write it to files

--- src/test/java/com/simonbaars/clonerefactor/types/JHotDraw.java
+++ src/test/java/com/simonbaars/clonerefactor/types/JHotDraw.java
@@ -3,16 +3,19 @@ package com.simonbaars.clonerefactor.types;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.simonbaars.clonerefactor.Main;
+import com.simonbaars.clonerefactor.context.enums.LocationType;
+import com.simonbaars.clonerefactor.context.enums.RelationType;
 import com.simonbaars.clonerefactor.core.util.SavePaths;
+import com.simonbaars.clonerefactor.datatype.map.CountMap;
 import com.simonbaars.clonerefactor.detection.model.DetectionResults;
 import com.simonbaars.clonerefactor.helper.Type1Test;
 import com.simonbaars.clonerefactor.refactoring.enums.RefactoringStrategy;
 import com.simonbaars.clonerefactor.scripts.intimals.IntimalsReader;
 import com.simonbaars.clonerefactor.scripts.intimals.model.PatternSequence;
+import com.simonbaars.clonerefactor.scripts.intimals.similarity.Intersects;
 import com.simonbaars.clonerefactor.scripts.intimals.similarity.Similarity;
 import com.simonbaars.clonerefactor.scripts.model.MetricsTable;
 import com.simonbaars.clonerefactor.settings.CloneType;
@@ -77,6 +80,34 @@ public class JHotDraw extends Type1Test {
 		List<Similarity> sims2 = new Similarity().determineSimilarities(patternSequences, cloneDetection.getClones(), false);
 		System.out.println(sims1);
 		System.out.println(sims2);
+		CountMap<Double> sims1SimilarityPerc = new CountMap<>();
+		CountMap<Double> sims2SimilarityPerc = new CountMap<>();
+		sims1.forEach(e -> sims1SimilarityPerc.increment(e.similarityPercentage()));
+		sims2.forEach(e -> sims2SimilarityPerc.increment(e.similarityPercentage()));
+		CountMap<Double> sims1IntersectPerc = new CountMap<>();
+		CountMap<Double> sims2IntersectPerc = new CountMap<>();
+		sims1.forEach(e -> sims1IntersectPerc.increment(e.intersectPercentage()));
+		sims2.forEach(e -> sims2IntersectPerc.increment(e.intersectPercentage()));
+		CountMap<Integer> sims1NumIntersect = new CountMap<>();
+		CountMap<Integer> sims2NumIntersect = new CountMap<>();
+		sims1.forEach(e -> sims1NumIntersect.increment(e.intersectNum()));
+		sims2.forEach(e -> sims2NumIntersect.increment(e.intersectNum()));
+		CountMap<LocationType> mostMatchedLocation = new CountMap<>();
+		CountMap<RelationType> mostMatchedRelation = new CountMap<>();
+		sims2.stream().filter(e -> e.similarityPercentage()>0D).forEach(e -> mostMatchedRelation.increment(e.getClone().getRelation().getType()));
+		sims2.stream().flatMap(e -> e.getMatches().stream()).filter(e -> e instanceof Intersects).map(e -> (Intersects)e).forEach(e -> mostMatchedLocation.increment(e.getClone().getLocationType()));
+		try {
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"sims1SimilarityPerc.txt"), sims1SimilarityPerc.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"sims2SimilarityPerc.txt"), sims2SimilarityPerc.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"sims1IntersectPerc.txt"), sims1IntersectPerc.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"sims2IntersectPerc.txt"), sims2IntersectPerc.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"sims1NumIntersect.txt"), sims1NumIntersect.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"sims2NumIntersect.txt"), sims2NumIntersect.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"mostMatchedLocation.txt"), mostMatchedLocation.toString());
+			writeStringToFile(new File(SavePaths.getMyOutputFolder()+"mostMatchedRelation.txt"), mostMatchedRelation.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 	
