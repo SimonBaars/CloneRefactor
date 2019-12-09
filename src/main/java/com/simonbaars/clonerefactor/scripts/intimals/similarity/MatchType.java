8b98681b25a8cfc63a3352edcfe277313146d865
Simon <simon.mailadres@gmail.com>
2019-12-09 12:04:23 +0100

    Match types

---
+++ src/main/java/com/simonbaars/clonerefactor/scripts/intimals/similarity/MatchType.java
@@ -0,0 +1,17 @@
+package com.simonbaars.clonerefactor.scripts.intimals.similarity;
+
+import com.simonbaars.clonerefactor.scripts.intimals.model.SimpleRange;
+
+public enum MatchType {
+	CLONEINPATTERN, PATTERNINCLONE, OVERFLOW, EXACTMATCH;
+
+	public static MatchType determine(SimpleRange patternRange, SimpleRange cloneRange) {
+		if(patternRange.equals(cloneRange))
+			return EXACTMATCH;
+		else if(patternRange.contains(cloneRange))
+			return CLONEINPATTERN;
+		else if(cloneRange.contains(patternRange))
+			return CLONEINPATTERN;
+		return OVERFLOW;
+	}
+}
